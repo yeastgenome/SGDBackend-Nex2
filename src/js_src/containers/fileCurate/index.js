ccf14cd46ed4580f24747813dfababcba8a216e2
felixgondwe <gondwefelix8@gmail.com>
2018-12-06 15:10:09 -0800

    minor changes

--- src/js_src/containers/fileCurate/index.js
+++ src/js_src/containers/fileCurate/index.js
@@ -1,21 +1,22 @@
+
+import React, {Component} from 'react';
+import FileCurateForm from '../../components/fileCurate/fileCurateForm';
+import CurateLayout from '../curateHome/layout';
+
 /**
- * purpose: File Curation container component
- * author: fgondwe
+ * @summary File Curation container component
+ * @author fgondwe
  * Notes:
  *      user should be able to update a readmefile, comments for instance.
  *      update readmefile automatically with s3 urls.
-*/
-import React, {Component} from 'react';
-import CurateLayout from '../curateHome/layout';
-
-
+ */
 class FileCurate extends Component {
   constructor(props){
     super(props);
   }
 
   render(){
-    return (<CurateLayout><p>File curate interface</p></CurateLayout>);
+    return (<CurateLayout><FileCurateForm /></CurateLayout>);
   }
 }
 
