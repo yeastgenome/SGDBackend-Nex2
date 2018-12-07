1249a787cc09d25bc924cc452a872e337468b1dc
felixgondwe <gondwefelix8@gmail.com>
2018-12-07 10:12:22 -0800

    added datetime component

--- src/js_src/components/fileCurate/fileCurateForm.js
+++ src/js_src/components/fileCurate/fileCurateForm.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import TextField from '../forms/textField';
 import StringField from '../forms/stringField';
-import { SMALL_COL_CLASS, LARGE_COL_CLASS } from '../../constants';
+import FormDatePicker from '../../components/formDatePicker';
 
 const FileCurateForm = () => {
   return(
     <div>
-      <StringField displayName={'Display Name'} />
-      <StringField displayName={'Status'} />
-      <StringField displayName={'keywords'} />
-      <StringField displayName={'Previous Filename'} />
-      <TextField displayName={'Description'} />
+      <div className={'row'} >
+        <div className={'columns small-6'}>
+          <StringField className={'columns small-6'} displayName={'Display Name'} />
+        </div>
+        <div className={'columns small-6'}>
+          <StringField className={'columns small-6'} displayName={'Status'} />      </div>
+        </div>
+
+      <div className={'row'}>
+        <div className={'columns small-6'}>
+          <StringField className={'columns small-6 medium-6'} displayName={'keywords'} />
+        </div>
+        <div className={'columns small-6'}>
+          <StringField className={'columns small-6 medium-6'} displayName={'Previous Filename'} />
+        </div>
+      </div>
+      <div className={'row'}>
+        <div className={'columns small-6'}><TextField  displayName={'Description'} /></div>
+        <div className={'columns small-6 small-offset-5'}></div>
+      </div>
+      <div className={'row'}>
+        <div className={'columns small-6'}><FormDatePicker /></div>
+        <div className={'columns small-6 small-offset-5'}></div>
+      </div>
 
     </div>
   );
