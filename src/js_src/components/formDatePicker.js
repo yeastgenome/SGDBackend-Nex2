bca8f10a708993dc661dc5a9a3e96a781a117bea
felixgondwe <gondwefelix8@gmail.com>
2018-12-07 10:12:22 -0800

    added datetime component

---
+++ src/js_src/components/formDatePicker.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+class FormDatePicker extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      startDate: moment()
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+  handleChange(date) {
+    this.setState({startDate: date});
+  }
+
+  render(){
+    return (<DatePicker selected={this.state.startDate} onChange={this.handleChange} />);
+  }
+}
+
+export default FormDatePicker;
