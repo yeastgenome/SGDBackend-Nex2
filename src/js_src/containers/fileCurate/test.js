598c9caad424d7c3ed33fb456c6a0f9814408376
felixgondwe <gondwefelix8@gmail.com>
2019-01-17 10:42:04 -0800

    minor changes

--- src/js_src/containers/fileCurate/test.js
+++
@@ -1,9 +0,0 @@
-import assert from 'assert';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-
-import { FileCurate } from './index';
-
-/*describe('FileCurate', () => {
-
-});*/
