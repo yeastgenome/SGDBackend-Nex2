4059ddf366cdb416b07127d99db8d199cec774e7
felixgondwe <gondwefelix8@gmail.com>
2019-03-01 14:49:58 -0800

    minor changes

--- src/views.py
+++ src/views.py
@@ -667,13 +667,19 @@ def disease_locus_details_all(request):
 
 @view_config(route_name='locus', renderer='json', request_method='GET')
 def locus(request):
-
+    #import pdb; pdb.set_trace()
     id = extract_id_request(request, 'locus', param_name="sgdid")
-    locus = get_locus_by_id(id)
-    if locus:
-        return locus.to_dict()
-    else:
+    try:
+        locus = get_locus_by_id(id)
+        if locus:
+            return locus.to_dict()
+        else:
+            return HTTPNotFound()
+    except Exception as e:
+        import pdb; pdb.set_trace()
+        print(e)
         return HTTPNotFound()
+
 
 @view_config(route_name='locus_tabs', renderer='json', request_method='GET')
 def locus_tabs(request):
@@ -737,6 +743,7 @@ def locus_expression_graph(request):
 
 @view_config(route_name='locus_literature_details', renderer='json', request_method='GET')
 def locus_literature_details(request):
+    import pdb; pdb.set_trace()
     id = extract_id_request(request, 'locus')
     locus = get_locus_by_id(id)
     if locus:
@@ -1432,7 +1439,7 @@ def healthcheck(request):
 
 
 # api portal with swagger
-@view_config(route_name='api_portal', renderer='json')
+'''@view_config(route_name='api_portal', renderer='json')
 def api_portal(request):
     request.response.headers.update({
        'Access-Control-Allow-Origin': '*',
@@ -1444,5 +1451,5 @@ def api_portal(request):
     with open(json_file) as f:
         data = json.load(f)
 
-    return data
+    return data '''
 
