221294422ebc10f2fc012e197d2bfb9d3b2bbeaa
felixgondwe <gondwefelix8@gmail.com>
2019-03-11 11:57:29 -0700

    - Add swagger spec - Needed for API documentation - No known side effects yet

--- src/views.py
+++ src/views.py
@@ -1436,7 +1436,7 @@ def healthcheck(request):
 
 
 # api portal with swagger
-'''@view_config(route_name='api_portal', renderer='json')
+@view_config(route_name='api_portal', renderer='json')
 def api_portal(request):
     request.response.headers.update({
        'Access-Control-Allow-Origin': '*',
@@ -1447,5 +1447,5 @@ def api_portal(request):
     with open(json_file) as f:
         data = json.load(f)
 
-    return data '''
+    return data
 
