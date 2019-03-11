7a37a745b3119f973e4959a22c73da890a72cb76
felixgondwe <gondwefelix8@gmail.com>
2019-03-11 11:57:29 -0700

    - Add swagger spec - Needed for API documentation - No known side effects yet

--- src/__init__.py
+++ src/__init__.py
@@ -169,7 +169,7 @@ def main(global_config, **settings):
 
     #swagger
     #config.add_static_view('static', 'static', cache_max_age=3600)
-    #config.add_route('api_portal', '/api', request_method='GET')
+    config.add_route('api_portal', '/api', request_method='GET')
     config.scan()
     config.add_static_view(name='assets', path='./build')
 
