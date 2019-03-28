32d04c0dfa91f00c8c0c6ba797f9d0f04b8481a7
felixgondwe <gondwefelix8@gmail.com>
2019-03-27 20:33:16 -0700

    file upload changes

--- src/helpers.py
+++ src/helpers.py
@@ -16,7 +16,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import re
-
 from .models import DBSession, Dbuser, Go, Referencedbentity, Keyword, Locusdbentity, FilePath, Edam, Filedbentity, FileKeyword, ReferenceFile, Disease, CuratorActivity
 from src.curation_helpers import ban_from_cache, get_curator_session
 
@@ -600,8 +599,9 @@ def get_file_delimiter(file_upload):
     '''
 
     if file_upload:
+        temp = file_upload.readline()
         dialect = csv.Sniffer().sniff(
-            file_upload.readline(), [',', '|', '\t', ';'])
+            temp, [',', '|', '\t', ';'])
         file_upload.seek(0)
         return dialect.delimiter
     else:
