d24dd39fe6f70346608b4fcfd13c11db1e870c96
felixgondwe <gondwefelix8@gmail.com>
2019-05-16 13:45:04 -0700

    minor changes

---
+++ src/js_src/containers/fileCurate/updateFile.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import CurateLayout from '../curateHome/layout';
+import { connect } from 'react-redux';
+/* eslint-disable no-debugger */
+
+//const BASE_CURATE_URL = 'curate/file';
+
+class FileCurateUpdate extends Component{
+  render(){
+    return(
+      <CurateLayout>
+        <div className='row'>
+          <p>File curate page</p>
+        </div>
+      </CurateLayout>
+    );
+  }
+
+}
+
+function mapStateToProps(state){
+  debugger;
+  return state;
+}
+
+export default connect(mapStateToProps)(FileCurateUpdate);
