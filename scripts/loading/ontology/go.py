859b986f75527afe88c6c025ad0cebcc537bc461
sweng66 <sweng@stanford.edu>
2019-10-30 11:26:59 -0700

    fixed the code for uploading files to S3

--- scripts/loading/ontology/go.py
+++ scripts/loading/ontology/go.py
@@ -343,7 +343,7 @@ def update_database_load_file_to_s3(nex_session, ontology_file, source_to_id, ed
     local_file = open(gzip_file, mode='rb')
 
     import hashlib
-    go_md5sum = hashlib.md5(local_file.read()).hexdigest()
+    go_md5sum = hashlib.md5(ontology_file.encode()).hexdigest()
     go_row = nex_session.query(Filedbentity).filter_by(md5sum = go_md5sum).one_or_none()
 
     if go_row is not None:
@@ -374,7 +374,8 @@ def update_database_load_file_to_s3(nex_session, ontology_file, source_to_id, ed
                 is_in_spell='0',
                 is_in_browser='0',
                 file_date=datetime.now(),
-                source_id=source_to_id['SGD'])
+                source_id=source_to_id['SGD'],
+                md5sum=go_md5sum)
 
 
 def write_summary_and_send_email(fw, update_log, to_delete_list):
