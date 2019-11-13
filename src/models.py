bd74909623016abc3cd44609b1b93fda94249e07
Sagar <sagar.jhaa@gmail.com>
2019-11-13 12:41:27 -0800

    Add interaction and disease summary to dict

--- src/models.py
+++ src/models.py
@@ -4870,6 +4870,8 @@ class Locusdbentity(Dbentity):
     def to_curate_dict(self):
         phenotype_summary = DBSession.query(Locussummary).filter_by(locus_id=self.dbentity_id, summary_type='Phenotype').one_or_none()
         regulation_summary = DBSession.query(Locussummary).filter_by(locus_id=self.dbentity_id, summary_type='Regulation').one_or_none()
+        interaction_summary = DBSession.query(Locussummary).filter_by(locus_id=self.dbentity_id, summary_type='Interaction').one_or_none()
+        disease_summary = DBSession.query(Locussummary).filter_by(locus_id=self.dbentity_id, summary_type='Disease').one_or_none()
         if not phenotype_summary:
             phenotype_summary = ''
             phenotype_summary_pmids = ''
@@ -4888,7 +4890,10 @@ class Locusdbentity(Dbentity):
             pmids = [str(x[0]) for x in pmids]
             regulation_summary_pmids = SEPARATOR.join(pmids)
             regulation_summary = regulation_summary.text
-
+        if interaction_summary:
+            interaction_summary = interaction_summary.text
+        if disease_summary:
+            disease_summary = disease_summary.text 
         aliases = DBSession.query(LocusAlias).filter(and_(LocusAlias.locus_id==self.dbentity_id, LocusAlias.alias_type.in_(['Uniform', 'Non-uniform', 'Retired name']))).all()
         aliases_list = []
         for x in aliases:
@@ -4927,7 +4932,9 @@ class Locusdbentity(Dbentity):
                 'phenotype_summary': phenotype_summary,
                 'phenotype_summary_pmids': phenotype_summary_pmids,
                 'regulation_summary': regulation_summary,
-                'regulation_summary_pmids': regulation_summary_pmids
+                'regulation_summary_pmids': regulation_summary_pmids,
+                'interaction_summary':interaction_summary,
+                'disease_summary':disease_summary
             },
             'basic': {
                 'aliases': aliases_list,
