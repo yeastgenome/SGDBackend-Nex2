68164e08de2c1cef2c4538c040c203e3247e4fe9
Shuai Weng <sweng@stanford.edu>
2020-02-10 11:06:19 -0800

    commented out the debugging print statement

--- scripts/loading/pathway/remove_html_tags.py
+++ scripts/loading/pathway/remove_html_tags.py
@@ -11,7 +11,7 @@ for x in all_pathways:
 
     if ">" in x.display_name or "<" in x.display_name:
         display_name = x.display_name.replace("<i>", "").replace("</i>", "")
-        print (x.dbentity_id, x.display_name, display_name)
+        # print (x.dbentity_id, x.display_name, display_name)
         nex_session.query(Dbentity).filter_by(dbentity_id=x.dbentity_id).update({ 'display_name': display_name })
 
 nex_session.commit()
