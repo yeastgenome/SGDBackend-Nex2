77107c583b18af9cc0a7e2088b994154327889d8
felixgondwe <gondwefelix8@gmail.com>
2020-02-11 22:21:49 -0800

    Modify log module for tests, es-script, curator_views

--- src/loading/scrapy/pages/spiders/pages_spider.py
+++ src/loading/scrapy/pages/spiders/pages_spider.py
@@ -1,12 +1,12 @@
 import scrapy
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
-from scrapy.utils.log import configure_logging
 from twisted.internet import reactor, defer
 from sqlalchemy import create_engine, and_
 import os
-import logging
 import traceback
+from custom_logger import setup_logger
+from pathlib import Path
 
 from src.models import Apo, DBSession, Dnasequenceannotation, Go, Locusdbentity, Phenotype, Referencedbentity, Straindbentity
 
@@ -15,12 +15,15 @@ HEADER_OBJ = {'X-Forwarded-Proto': 'https'}
 engine = create_engine(os.environ['NEX2_URI'], pool_recycle=3600, pool_size=2)
 DBSession.configure(bind=engine)
 
-if 'WORKER_LOG_FILE' in list(os.environ.keys()):
-    LOG_FILE = os.environ['WORKER_LOG_FILE']
-    logging.basicConfig(
-        filename=LOG_FILE, format='%(asctime)s %(levelname)s %(message)s')
-logger = logging.getLogger(__name__)
-logger.setLevel(level=logging.INFO)
+# setup logs
+format_str = '%(asctime)s: %(levelname)-5.5s: %(name)s: %(funcName)s: %(message)s'
+file_path = os.environ.get("WORKER_LOG_FILE", "/worker.log")
+dev_env = os.environ.get("ENV", "prod")
+mod_path = file_path
+if dev_env == "dev" or dev_env == "test":
+    mod_path = os.path.join(Path.cwd(), file_path[1:])
+    
+spider_logger = setup_logger('spider_logger', mod_path, format_str)
 
 
 # init spiders
@@ -39,7 +42,7 @@ class BaseSpider(scrapy.Spider):
             index = entities.index(entity)
             if (index % 100 == 0):
                 percent_done = str(float(index) / float(len(entities)) * 100)
-                self.logger.info('CHECKIN STATS: ' + percent_done + '% of current index complete')
+                spider_logger.info('CHECKIN STATS: ' + percent_done + '% of current index complete')
             urls = entity.get_all_cache_urls(True)
             for url in urls:
                 yield scrapy.Request(url=url, headers=HEADER_OBJ, method='PURGE', callback=self.parse)
@@ -49,14 +52,14 @@ class BaseSpider(scrapy.Spider):
         # # to debug latency
         # latency = response.request.meta['download_latency']
         if response.status != 200:
-            self.logger.error('error on ' + response.url)
+            spider_logger.error('error on ' + response.url)
 
 
 class GenesSpider(BaseSpider):
     name = 'genes'
 
     def get_entities(self):
-        self.logger.info('getting genes')
+        spider_logger.info('getting genes')
         attempts = 0
         while attempts < 3:
             try:
@@ -71,9 +74,8 @@ class GenesSpider(BaseSpider):
                     dbentity_ids_to_so[gis[0]] = gis[1]
                 all_genes = DBSession.query(Locusdbentity).filter(Locusdbentity.dbentity_id.in_(list(dbentity_ids)), Locusdbentity.dbentity_status == 'Active').all()
                 break
-            except StatementError:
-                traceback.print_exc()
-                log.info('DB error corrected. Rollingback previous error in db connection')
+            except Exception:
+                spider_logger.error('DB Error', exc_info=True)
                 DBSession.rollback()
                 attempts += 1
         return all_genes
@@ -83,14 +85,14 @@ class GoSpider(BaseSpider):
     name = 'go'
 
     def get_entities(self):
-        self.logger.info('getting gos')
+        spider_logger.info('getting gos')
         attempts = 0
         while attempts < 3:
             try:
                 gos = DBSession.query(Go).all()
                 break
-            except:
-                traceback.print_exc()
+            except Exception:
+                spider_logger.error('DB Error', exc_info=True)
                 DBSession.rollback()
                 attempts += 1
         return gos
@@ -100,36 +102,49 @@ class ObservableSpider(BaseSpider):
     name = 'observable'
 
     def get_entities(self):
-        self.logger.info('getting observables')
+        spider_logger.info('getting observables')
         attempts = 0
         while attempts < 3:
             try:
                 observables = DBSession.query(Apo).filter_by(apo_namespace="observable").all()
                 break
-            except:
-                traceback.print_exc()
+            except Exception:
+                spider_logger.error('DB Error', exc_info=True)
                 DBSession.rollback()
                 attempts += 1
         return observables
+
 
 class PhenotypeSpider(BaseSpider):
     name = 'phenotype'
 
     def get_entities(self):
-        self.logger.info('getting phenotypes')
+        spider_logger.info('getting phenotypes')
         attempts = 0
         while attempts < 3:
             try:
                 phenotypes = DBSession.query(Phenotype).all()
                 break
-            except:
-                traceback.print_exc()
+            except Exception:
+                spider_logger.error('DB Error', exc_info=True)
                 DBSession.rollback()
                 attempts += 1
         return phenotypes
 
-configure_logging()
+
+class YeastgenomeSite(BaseSpider):
+
+    def get_entities(self):
+       return ['https://www.yeastgenome.org/locus/S000001855']
+    
+
 runner = CrawlerRunner()
+
+
+def crawl_test():
+    # yield runner.crawl(YeastgenomeSite)
+    # reactor.stop
+    spider_logger.info('Crawler is logging')
 
 
 @defer.inlineCallbacks
@@ -139,6 +154,7 @@ def crawl():
     yield runner.crawl(PhenotypeSpider)
     yield runner.crawl(GenesSpider)
     reactor.stop()
+
 
 if __name__ == '__main__':
     crawl()
