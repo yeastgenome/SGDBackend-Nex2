39a8a024aa1f3778446ad2416ff5f2039028a9fc
Shuai Weng <sweng@stanford.edu>
2020-02-28 12:16:04 -0800

    fixed PREVIEW URL

--- src/js_src/constants.js
+++ src/js_src/constants.js
@@ -2,9 +2,9 @@ export const SEARCH_API_ERROR_MESSAGE = 'There was a problem connecting to the s
 export const LARGE_COL_CLASS = 'columns small-8 medium-9';
 export const NON_HIGHLIGHTED_FIELDS = ['sourceHref', 'href', 'category', 'homologs', 'paralogs', 'orthologs', 'homologs.symbol', 'homologs.panther_family'];
 export const SMALL_COL_CLASS = 'columns small-4 medium-3';
-let URL = process.env.PREVIEW_URL
+let URL = process.env.PREVIEW_URL;
 if (URL != 'https://preview.qa.yeastgenome.org') {
-    URL = 'https://preview.yeastgenome.org'
+  URL = 'https://preview.yeastgenome.org';
 }
 export const PREVIEW_URL = URL
 export const ml_12 = {marginLeft:'12px'};
