44a276fa9b39af08fc5dfd8adac2a51ff24a26cb
Shuai Weng <sweng@stanford.edu>
2020-04-15 08:22:55 -0700

    fixed to work with newly fixed datalist

--- src/js_src/containers/phenotype/autocompleteSection.js
+++ src/js_src/containers/phenotype/autocompleteSection.js
@@ -48,9 +48,10 @@ class AutocompleteSection extends Component {
             </div>
           </div>
           <div className='row'>
-	    {
+            {
               this.state.options.length > 0?
               <DataList options={this.state.options} id={this.props.id} left={this.props.value1} right={this.props.value2} selectedIdName={this.props.selectedIdName} onOptionChange={this.props.onOptionChange} selectedId={this.props.selectedId} placeholder={this.props.placeholder} setNewValue={this.props.setNewValue} />
+              :''
             }
           </div>
         </div>
