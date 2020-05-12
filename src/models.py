9baf304f4c2c80032243c7a5ad1a71311d9ead65
Shuai Weng <sweng@stanford.edu>
2020-05-12 00:10:02 -0700

    testing

--- src/models.py
+++ src/models.py
@@ -872,7 +872,7 @@ class Chebi(Base):
         response = urlopen(url)
         res = response.read()
         if len(res) > 0:
-            d = int(len(res)*200/300);
+            d = int(len(res)*200/800);
             if d < 200:
                 d = 200
             return url + "&dimensions=" + str(d)
