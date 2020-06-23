2bd9a30e67ef5211a1915a63129e198f5563b43d
sweng66 <sweng@stanford.edu>
2020-06-23 15:53:53 -0700

    fixes to phenotype curation page to handle reporters with special characters properly; adding new script for loading chebi to pathway data (#285)
    * adding script for loading chemical_Pathway_mapping data
    
    * fixed to work with reporters with special characters properly

--- src/phenotype_helpers.py
+++ src/phenotype_helpers.py
@@ -109,6 +109,10 @@ def insert_allele(curator_session, CREATED_BY, source_id, allele):
 
 def insert_reporter(curator_session, CREATED_BY, source_id, reporter):
 
+    r = curator_session.query(Reporter).filter_by(display_name=reporter).one_or_none()
+    if r is not None:
+        return r.reporter_id
+    
     isSuccess = False
     returnValue = ""
     reporter_id = None
@@ -820,8 +824,9 @@ def add_phenotype_annotations(request):
         return HTTPOk(body=json.dumps({'success': success_message, 'phenotype': "PHENOTYPE"}), content_type='text/json')
     except Exception as e:
         return HTTPBadRequest(body=json.dumps({'error': str(e)}), content_type='text/json')
-
-
+    finally:
+        if curator_session:
+            curator_session.remove()
 
 def get_apo_by_id(apo_id):
 
@@ -1354,7 +1359,9 @@ def update_phenotype_annotations(request):
         return HTTPOk(body=json.dumps({'success': success_message, 'phenotype': "PHENOTYPE"}), content_type='text/json')
     except Exception as e:
         return HTTPBadRequest(body=json.dumps({'error': str(e) + "<br>DONE reporting ERROR!"}), content_type='text/json')
-
+    finally:
+        if curator_session:
+            curator_session.remove()
 
 def delete_phenotype_annotations(request):
 
@@ -1440,7 +1447,10 @@ def delete_phenotype_annotations(request):
         return HTTPOk(body=json.dumps({'success': success_message, 'phenotype': "PHENOTYPE"}), content_type='text/json')
     except Exception as e:
         return HTTPBadRequest(body=json.dumps({'error': str(e)+"<br>DONE reporting ERROR!"}), content_type='text/json')
-
+    finally:
+        if curator_session:
+            curator_session.remove()
+            
 def phenotype_to_dict(row):
 
     dbentity = DBSession.query(Locusdbentity).filter_by(dbentity_id=row.dbentity_id).one_or_none()
