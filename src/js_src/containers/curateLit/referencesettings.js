4913f8a03ac092a599db9c44dda649cb2069fdd9
Sagar <sagar.jhaa@gmail.com>
2020-06-29 16:47:57 -0700

    Get data from different forms to send over to BE.

--- src/js_src/containers/curateLit/referencesettings.js
+++ src/js_src/containers/curateLit/referencesettings.js
@@ -8,12 +8,34 @@ class ReferenceSettings extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {}
+      data: {},
+      changeData: {},
+      table_primary_key: {
+        'Bindingmotifannotation': 'annotation_id',
+        'Literatureannotation': 'annotation_id',
+      }
     };
     this.handleOnSuccess = this.handleOnSuccess.bind(this);
   }
   handleOnSuccess(data) {
     this.setState({ data });
+  }
+
+  handleIndividualFormSubmit(table_name){
+    console.log(this.state.changeData[table_name]);
+  }
+
+  getConfirmationMessage(table_name=undefined){
+    // var message = '';
+    if (table_name == undefined){
+      //All the table and data
+    }
+    else{
+      var data = this.state.changeData[table_name];
+      for(var index =0;index<data.length;index++){
+        console.log(data[index]);
+      } 
+    }
   }
 
   componentDidMount() {
@@ -30,6 +52,161 @@ class ReferenceSettings extends Component {
         this.setState({ data });
       });
   }
+
+  handleTransferChange(table_name, e) {
+    var primary_key = this.state.table_primary_key[table_name];
+    var primary_key_value = e.target.dataset.primaryIndex;
+    var new_pmid_value = e.target.value;
+
+    // var textbox = e.target.closest('input');
+    // console.log(textbox);
+    // textbox.value = '';
+
+    if (table_name in this.state.changeData) {
+      // key already in that table
+      //have to take care of others
+
+      var currentStateValue = Object.assign({}, this.state.changeData[table_name]);
+      currentStateValue[primary_key_value] = {
+        [primary_key]: primary_key_value,
+        'pmid': new_pmid_value,
+      };
+
+      this.setState(() => {
+        return {
+          changeData: {
+            [table_name]: currentStateValue
+          }
+        };
+      },() => console.log(this.state.changeData));
+
+      // if (primary_key_value in this.state.changeData[table_name]){
+      //   this.setState(() => {
+      //     return {
+      //       changeData: {
+      //         [table_name]: {
+      //           primary_key_value :
+      //           {
+      //             [primary_key]: primary_key_value,
+      //             'delete': e.target.checked,
+      //           },
+      //         }
+      //       }
+      //     };
+      //   });
+      // }
+      // //key not in that table
+      // else{
+      //   this.setState(() => {
+      //     return {
+      //       changeData: {
+      //         [table_name]: {
+      //           primary_key_value :
+      //           {
+      //             [primary_key]: primary_key_value,
+      //             'delete': e.target.checked,
+      //           },
+      //         }
+      //       }
+      //     };
+      //   });
+      // }
+    }
+    else {
+      this.setState(() => {
+        return {
+          changeData: {
+            [table_name]: {
+              [primary_key_value] :
+              {
+                [primary_key]: primary_key_value,
+                'pmid': new_pmid_value,
+              },
+            }
+          }
+        };
+      },() => console.log(this.state.changeData));
+    }
+  }
+
+
+
+  handleDeleteChange(table_name, e) {
+    var primary_key = this.state.table_primary_key[table_name];
+    var primary_key_value = e.target.dataset.primaryIndex;
+
+    // var textbox = e.target.closest('input');
+    // console.log(textbox);
+    // textbox.value = '';
+
+    if (table_name in this.state.changeData) {
+      // key already in that table
+      //have to take care of others
+
+      var currentStateValue = Object.assign({}, this.state.changeData[table_name]);
+      currentStateValue[primary_key_value] = {
+        [primary_key]: primary_key_value,
+        'delete': e.target.checked,
+      };
+
+      this.setState(() => {
+        return {
+          changeData: {
+            [table_name]: currentStateValue
+          }
+        };
+      },() => console.log(this.state.changeData));
+
+      // if (primary_key_value in this.state.changeData[table_name]){
+      //   this.setState(() => {
+      //     return {
+      //       changeData: {
+      //         [table_name]: {
+      //           primary_key_value :
+      //           {
+      //             [primary_key]: primary_key_value,
+      //             'delete': e.target.checked,
+      //           },
+      //         }
+      //       }
+      //     };
+      //   });
+      // }
+      // //key not in that table
+      // else{
+      //   this.setState(() => {
+      //     return {
+      //       changeData: {
+      //         [table_name]: {
+      //           primary_key_value :
+      //           {
+      //             [primary_key]: primary_key_value,
+      //             'delete': e.target.checked,
+      //           },
+      //         }
+      //       }
+      //     };
+      //   });
+      // }
+    }
+    else {
+      var checked = e.target.checked;
+      this.setState(() => {
+        return {
+          changeData: {
+            [table_name]: {
+              [primary_key_value] :
+              {
+                [primary_key]: primary_key_value,
+                'delete': checked,
+              },
+            }
+          }
+        };
+      },() => console.log(this.state.changeData));
+    }
+  }
+
 
   renderReferenceSettings() {
     let reasonDelete = [
@@ -66,23 +243,24 @@ class ReferenceSettings extends Component {
     return headers;
   }
 
-  get_columns(table_values) {
+  get_columns(table_name, table_values) {
     var results = [];
     for (var key in table_values) {
       var value = table_values[key];
+      var primary_key = this.state.table_primary_key[table_name];
       var column_names = Object.keys(value);
       var result = [];
       column_names.forEach(item => {
         result.push(<td>{value[item]}</td>);
       });
-      result.push(<td><input  placeholder='Enter PMID' type='number'/></td>);
-      result.push(<td><input type='Checkbox' name='Delete' /></td>);
+      result.push(<td><input placeholder='Enter PMID' type='number' data-primary-index={value[primary_key]} onChange={(e) => this.handleTransferChange(table_name, e)} /></td>);
+      result.push(<td><input type='Checkbox' name='Delete' data-primary-index={value[primary_key]} onChange={(e) => this.handleDeleteChange(table_name, e)} /></td>);
       results.push(<tr>{result}</tr>);
     }
     return results;
   }
 
-  handleSubmit(event){
+  handleSubmit(event) {
     event.preventDefault();
     console.log('handle  submit');
     console.log(event);
@@ -106,37 +284,37 @@ class ReferenceSettings extends Component {
               return undefined;
               //<div key={`${table_name}_${index + 1}`}>{table_name}</div>;
             }
-            const data = this.get_columns(table_values);
-            
+            const fields = this.get_columns(table_name, table_values);
+
             var value = table_values[0];
             var column_names = Object.keys(value);
             var column_headers = this.get_headers(column_names);
 
             return (
               <div key={table_name}>
-              <h4>{table_name}</h4>
-              <form  onSubmit={this.handleSubmit} id={`form_${table_name.replace(' ','_').toLowerCase()}`}>
-              <table key={`${table_name}_${index + 1}`}>
-                <thead>
-                  <tr key={`${table_name}_${index + 2}`}>
-                    {column_headers}
-                  </tr>
-                </thead>
+                <h4>{table_name}</h4>
+                <form id={`form_${table_name.replace(' ', '_').toLowerCase()}`}>
+                  <table key={`${table_name}_${index + 1}`}>
+                    <thead>
+                      <tr key={`${table_name}_${index + 2}`}>
+                        {column_headers}
+                      </tr>
+                    </thead>
 
-                 <tbody key={`${table_name}_${index+3}`}>
-                  {data}
-                </tbody>
-              </table>
-              <button type='submit' className='button'>Submit</button>
-              <hr />
-              </form>
+                    <tbody key={`${table_name}_${index + 3}`}>
+                      {fields}
+                    </tbody>
+                  </table>
+                  <button type='button' className='button' onClick={() => this.handleIndividualFormSubmit(table_name)}>Submit</button>
+                  <hr />
+                </form>
               </div>
             );
           })
         }
 
         {
-         !renderData && <div>Not related to annotations</div> 
+          !renderData && <div>Not related to annotations</div>
         }
 
         {/* <pre style={{whiteSpace:'pre-wrap'}}>{JSON.stringify(this.state.data,null,2)}</pre> */}
