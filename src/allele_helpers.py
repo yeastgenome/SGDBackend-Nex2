67ad32e73d9361f32c1d9caff9669af640d43037
Shuai Weng <sweng@stanford.edu>
2020-10-30 22:59:16 -0700

    testing for deletion action...

--- src/allele_helpers.py
+++ src/allele_helpers.py
@@ -1136,11 +1136,6 @@ def delete_allele_data(request):
             deleted = 1
         if deleted == 1:
             success_message = success_message + "<br>" + "The allele_reference row(s) have been deleted. "
-
-
-        return HTTPBadRequest(body=json.dumps({'error': success_message}), content_type='text/json')
-
-    
             
         ## delete literatureannotation
         
@@ -1152,6 +1147,11 @@ def delete_allele_data(request):
         if deleted == 1:
             success_message = success_message + "<br>" + "The literatureannotation row(s) have been deleted. "
 
+
+        return HTTPBadRequest(body=json.dumps({'error': success_message}), content_type='text/json')
+
+
+    
         ## update phenotypeannotation and set allele_id to null
 
         all_pa = curator_session.query(Phenotypeannotation).filter_by(allele_id=allele_id).all()
