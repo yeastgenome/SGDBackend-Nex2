62af9c12de5e118809b58cc1f115ecf525ab5bb5
Shuai Weng <sweng@stanford.edu>
2020-11-03 10:50:09 -0800

    adding preview link

--- src/js_src/containers/allele/editAllele.js
+++ src/js_src/containers/allele/editAllele.js
@@ -157,10 +157,9 @@ class EditAllele extends Component {
     
   displayForm() {
     // let previewUrl = `${PREVIEW_URL}/allele/${this.state.allele_name}`;
-    let previewUrl = `${PREVIEW_URL}`;  
+    // <a href='{previewUrl}'>Preview this Allele page</a>
     return (
       <div>
-        <a href='{previewUrl}'>Preview this Allele page</a>
         <form onSubmit={this.handleUpdate} ref='form'>
           <input name='sgdid' value={this.props.allele.sgdid} className="hide" />
           <OneAllele allele={this.props.allele} onOptionChange={this.handleChange} />
