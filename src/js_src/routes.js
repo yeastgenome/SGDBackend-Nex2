119ee52cd82edba2ccc74afccdbaaeddcdf65efd
sweng66 <sweng@stanford.edu>
2021-03-03 22:48:26 -0800

    new code for dataset CI

--- src/js_src/routes.js
+++ src/js_src/routes.js
@@ -57,6 +57,11 @@ import EditLitGuide from './containers/litguide/edit';
 import SearchFileMetadata from './containers/fileMetadata/search';
 import EditFileMetadata from './containers/fileMetadata/edit';
 import UploadSupplFiles from './containers/supplFiles/upload';
+import LoadDataset from './containers/dataset/load';
+import EditDataset from './containers/dataset/edit';
+import EditDatasetSample from './containers/dataset/editSample';
+import EditDatasetTrack from './containers/dataset/editTrack';
+import SearchDataset from './containers/dataset/search';
 
 //TODO: Fix the Routes.
 export default (
@@ -91,7 +96,12 @@ export default (
           <Route component={requireAuthentication(SearchAllele)} path='/search_allele' />
           <Route component={requireAuthentication(SearchFileMetadata)} path='/search_file_metadata' />
           <Route component={requireAuthentication(EditFileMetadata)} path='/edit_file_metadata/:id' />
-          <Route component={requireAuthentication(UploadSupplFiles)} path='/upload_suppl_files' />	     
+          <Route component={requireAuthentication(UploadSupplFiles)} path='/upload_suppl_files' />
+          <Route component={requireAuthentication(LoadDataset)} path='/load_dataset' />
+          <Route component={requireAuthentication(EditDataset)} path='/curate/dataset/:id' />
+          <Route component={requireAuthentication(EditDatasetSample)} path='/curate/datasetsample/:id' />
+          <Route component={requireAuthentication(EditDatasetTrack)} path='/curate/datasettrack/:id' />
+          <Route component={requireAuthentication(SearchDataset)} path='/search_dataset' />	     
           <Route component={requireAuthentication(LitGuide)} path='/litguide_todo' />
           <Route component={requireAuthentication(AddLitGuide)} path='/add_litguide' />
           <Route component={requireAuthentication(EditLitGuide)} path='/edit_litguide' />
