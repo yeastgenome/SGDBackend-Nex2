777fc0eb2114588905c5b52c566caad327bab3df
sweng66 <sweng@stanford.edu>
2021-06-24 15:07:36 -0700

    script for dumping gene/pmid pair for NCBI

---
+++ scripts/dumping/ncbi/dump_gene_pmid_pair.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+import logging
+## import tarfile
+import os
+import sys
+import json
+import boto
+from boto.s3.key import Key
+import boto3
+import transaction
+# import gzip
+# import shutil
+from src.models import Referencedbentity, Locusdbentity, Literatureannotation
+from scripts.loading.database_session import get_session
+from src.helpers import upload_file
+
+__author__ = 'sweng66'
+
+logging.basicConfig(format='%(message)s')
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+S3_ACCESS_KEY = os.environ['S3_ACCESS_KEY']
+S3_SECRET_KEY = os.environ['S3_SECRET_KEY']
+S3_BUCKET = os.environ['S3_BUCKET']
+S3_BUCKET2 = os.environ['ARCHIVE_S3_BUCKET']
+
+CREATED_BY = os.environ['DEFAULT_USER']
+
+datafile = "scripts/dumping/ncbi/data/gene2pmid.tab"
+
+s3_archive_dir = "curation/literature/"
+
+obsolete_pmids = [25423496, 27651461, 28068213, 31088842]
+
+def dump_data():
+
+    nex_session = get_session()
+
+    datestamp = str(datetime.now()).split(" ")[0]
+
+    log.info(str(datetime.now()))
+    log.info("Getting basic data from the database...")
+    
+    dbentity_id_to_gene = dict([(x.dbentity_id, x.systematic_name)
+                                 for x in nex_session.query(Locusdbentity).all()])
+    reference_id_to_pmid = dict([(x.dbentity_id, x.pmid)
+                                 for x in nex_session.query(Referencedbentity).all()])
+
+    fw = open(datafile, "w")
+
+    gene_to_pmid_list = {}
+    for x in nex_session.query(Literatureannotation).filter_by(topic='Primary Literature').order_by(Literatureannotation.dbentity_id, Literatureannotation.reference_id).all():
+        if x.dbentity_id not in dbentity_id_to_gene:
+            continue
+        if x.reference_id not in reference_id_to_pmid or reference_id_to_pmid[x.reference_id] in obsolete_pmids:
+            continue
+        pmid = reference_id_to_pmid[x.reference_id]
+        if pmid	is None:
+            continue
+        gene = dbentity_id_to_gene[x.dbentity_id]
+        pmid_list = []
+        if gene in gene_to_pmid_list:
+            pmid_list = gene_to_pmid_list[gene]
+        
+        if pmid not in pmid_list:
+            pmid_list.append(pmid)
+            gene_to_pmid_list[gene] = pmid_list
+        
+    for gene in sorted (gene_to_pmid_list.keys()):
+        pmids = gene_to_pmid_list[gene]
+        pmids.sort()
+        for pmid in pmids:
+            fw.write(gene + "\t" + str(pmid) + "\t" + x.topic + "\n")
+    fw.close()
+
+    upload_file_to_latest(datafile)
+    
+def upload_file_to_latest(datafile):
+
+    file = open(datafile, "rb")
+    
+    filename = "latest/" + datafile.split('/')[-1]
+    
+    s3_path = filename
+    conn = boto.connect_s3(S3_ACCESS_KEY, S3_SECRET_KEY)
+    bucket = conn.get_bucket(S3_BUCKET)
+    k = Key(bucket)
+    k.key = s3_path
+    k.set_contents_from_file(file, rewind=True)
+    k.make_public()
+    transaction.commit()
+        
+if __name__ == '__main__':
+
+    dump_data()
