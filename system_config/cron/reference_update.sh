b7fa6817310569c4d03279777ad431b7c74f1673
sweng66 <sweng@stanford.edu>
2021-06-25 09:33:47 -0700

    adding gene2pmid mapping script

--- system_config/cron/reference_update.sh
+++ system_config/cron/reference_update.sh
@@ -6,3 +6,4 @@ source /data/envs/sgd3/bin/activate
 source prod_variables.sh 
 python scripts/loading/reference/reference_update.py
 python scripts/loading/reference/reference_display_name_update.py
+python scripts/dumping/ncbi/dump_gene_pmid_pair.py
