5fc55ecf0d5d2c6e2da9456b96f3b0cefff8aa56
sweng66 <sweng@stanford.edu>
2021-06-28 10:41:36 -0700

    fixed setup.py to work for both new/old pip versions

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup, find_packages
-# from pip.download import PipSession
-# from pip.req import parse_requirements
-from pip._internal.download import PipSession
-from pip._internal.req import parse_requirements
+# from pip._internal.download import PipSession
+# from pip._internal.req import parse_requirements
+try:
+    from pip.req import parse_requirements
+except ImportError:
+    from pip._internal.req import parse_requirements
 
-install_reqs = parse_requirements("requirements.txt", session=PipSession())
-requires = [str(ir.req) for ir in install_reqs]
+# install_reqs = parse_requirements("requirements.txt", session=PipSession())
+# requires = [str(ir.req) for ir in install_reqs]
+
+install_reqs = parse_requirements("requirements.txt", session=False)
+requires = None
+try:
+    requires = [str(ir.req) for ir in install_reqs]
+except:
+    requires = [str(ir.requirement) for ir in install_reqs]
 
 setup(name='SGDBackend',
       version='0.0',
