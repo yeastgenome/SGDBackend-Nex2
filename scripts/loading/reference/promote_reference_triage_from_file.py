1f1a66b5fa29d455c36f43124b006edb0413be0c
sweng66 <sweng@stanford.edu>
2021-07-26 11:51:54 -0700

    fixed interfaces & scripts to avoid dup'ed DOI (#358)
    * check to not add dup'ed doi
    
    * check to not add dup'ed doi
    
    * check to not add dup'ed doi
    
    * check to not add dup'ed doi
    
    * check to skip dup'ed doi
    
    * check to skip dup'ed doi
    
    * check to skip dup'ed doi
    
    * fixed to skip dup'ed doi
    
    * fixed to skip dup'ed doi
    
    * fixed email

--- scripts/loading/reference/promote_reference_triage_from_file.py
+++ scripts/loading/reference/promote_reference_triage_from_file.py
@@ -31,6 +31,12 @@ def add_paper(pmid, created_by, date_created):
 
     record = get_pubmed_record(str(pmid))
 
+    doi = record.get('doi')
+    if doi:
+        ref = DBSession.query(Referencedbentity).filter_by(doi=doi).one_or_none()
+        if ref:
+            raise ValueError('The DOI for this pubmed is already in the database.')
+    
     ncbi = DBSession.query(Source).filter_by(format_name='NCBI').one_or_none()
     source_id = ncbi.source_id
 
