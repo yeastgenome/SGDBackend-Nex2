8b16c51944e8b9148002c0fc645b97e0402163bd
sweng66 <sweng@stanford.edu>
2021-09-28 12:58:13 -0700

    fixed to get around with the missing go evidence code

--- src/models.py
+++ src/models.py
@@ -7598,10 +7598,11 @@ class Goannotation(Base):
 
         alias = DBSession.query(EcoAlias).filter_by(eco_id=self.eco_id).all()
 
-	experiment_name = None
+        experiment_name = None
 
-	if len(alias) > 0:
-           experiment_name = alias[0].display_name
+        if len(alias) > 0:
+            experiment_name = alias[0].display_name
+            
         for alia in alias:
             if len(experiment_name) > len(alia.display_name):
                 experiment_name = alia.display_name
@@ -7630,9 +7631,10 @@ class Goannotation(Base):
             go = self.go
 
         alias = DBSession.query(EcoAlias).filter_by(eco_id=self.eco_id).all()
-	experiment_name = None
-	if len(alias) > 0:
-           experiment_name = alias[0].display_name
+
+        experiment_name = None
+        if len(alias) > 0:
+            experiment_name = alias[0].display_name
 
         for alia in alias:
             if len(experiment_name) > len(alia.display_name):
