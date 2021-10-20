e8f142a2af098683fe1d1e6c3bab0070f25307d9
sweng66 <sweng@stanford.edu>
2021-10-20 09:36:00 -0700

    fixed to get rid of special characters

--- scripts/dumping/curation/dump_gff.py
+++ scripts/dumping/curation/dump_gff.py
@@ -435,6 +435,8 @@ def do_escape(text):
     text = text.replace(" ", "%20").replace("(", "%28").replace(")", "%29")
     text = text.replace(",", "%2C")
     text = text.replace(";", "%3B")
+    text = text.replace('“', '"').replace('”', '"').replace("’", "'")
+    text = text.replace('α', 'alpha').replace('β', 'beta')
     text = text.rstrip()
     return text
 
