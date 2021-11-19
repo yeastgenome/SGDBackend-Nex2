72c02d84a01386a8577322b44c5be3ee61fff95d
sweng66 <sweng@stanford.edu>
2021-11-19 10:57:41 -0800

    adding a row to add any new papers for complex to literatureannotation table

--- system_config/cron/go_update.sh
+++ system_config/cron/go_update.sh
@@ -6,6 +6,7 @@ source prod_variables.sh
 python scripts/loading/ontology/go.py
 python scripts/loading/go/load_gpad.py 'manually curated'
 python scripts/loading/go/load_gpad.py computational
+python scripts/loading/complex/addMissingLiterature.py
 python scripts/dumping/curation/dump_go_annotations.py 
 python scripts/dumping/curation/dump_gpad.py
 python scripts/dumping/curation/dump_gpi.py
