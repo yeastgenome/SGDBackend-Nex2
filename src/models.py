7f7ee6208bb5a50476536a3889044a30d08d5060
sweng66 <sweng@stanford.edu>
2021-11-22 13:08:02 -0800

    testing

--- src/models.py
+++ src/models.py
@@ -4618,23 +4618,27 @@ class Locusdbentity(Dbentity):
             "qualifier": self.qualifier,
             "bioent_status": self.dbentity_status,
             "description": self.description,
-            "name_description": self.name_description,
-            "paralogs": self.paralogs_to_dict(),
-            "complements": self.complements_to_dict(),
-            "urls": [],
-            "protein_overview": self.protein_overview_to_dict(),
-            "go_overview": self.go_overview_to_dict(),
-            "pathways": [],
-            "alleles": [],
-            "phenotype_overview": self.phenotype_overview_to_dict(),
-            "interaction_overview": self.interaction_overview_to_dict(),
-            "paragraph": {
-                "date_edited": None
-            },
-            "literature_overview": self.literature_overview_to_dict(),
-            "disease_overview": self.disease_overview_to_dict(),
-            "ecnumbers": []    
+            "name_description": self.name_description
         }
+
+        return obj
+    
+            # "paralogs": self.paralogs_to_dict(),
+            # "complements": self.complements_to_dict(),
+            # "urls": [],
+            #"protein_overview": self.protein_overview_to_dict(),
+            #"go_overview": self.go_overview_to_dict(),
+            #"pathways": [],
+            #"alleles": [],
+            #"phenotype_overview": self.phenotype_overview_to_dict(),
+            #"interaction_overview": self.interaction_overview_to_dict(),
+            #"paragraph": {
+            #    "date_edited": None
+            #},
+            #"literature_overview": self.literature_overview_to_dict(),
+            #"disease_overview": self.disease_overview_to_dict(),
+            #"ecnumbers": []    
+        #}
 
         return obj
     
