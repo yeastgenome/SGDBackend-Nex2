3ec135000903ffa49e002c43af20cd3fc97c79f0
sweng66 <sweng@stanford.edu>
2022-02-04 14:19:15 -0800

    testing

--- system_config/cron/ontology_update.sh
+++ system_config/cron/ontology_update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cd /data/www/SGDBackend-NEX2/current
-source /data/envs/sgd3/bin/activate 
+cd /data/www/SGDBackend-NEX2
+source /data/www/venv/bin/activate 
 source prod_variables.sh 
 python scripts/loading/ontology/ec.py
 python scripts/loading/dbxref/update_ec.py
