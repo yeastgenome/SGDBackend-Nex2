da4848be4a9ec5c4dd96987d9de09d0119f77f6b
sweng66 <sweng@stanford.edu>
2022-02-07 11:25:48 -0800

    testing

--- scripts/loading/ontology/so.py
+++ scripts/loading/ontology/so.py
@@ -84,7 +84,7 @@ def load_new_data(nex_session, data, source_to_id, soid_to_so, ro_id, so_id_to_a
             y = soid_to_so[x['id']]
             so_id = y.so_id
             if y.is_obsolete is True:
-                y.is_obsolete = '0'
+                y.is_obsolete = 'false'
                 nex_session.add(y)
                 nex_session.flush()
                 update_log['updated'] = update_log['updated'] + 1
@@ -113,7 +113,7 @@ def load_new_data(nex_session, data, source_to_id, soid_to_so, ro_id, so_id_to_a
                          term_name = x['term_orig'],
                          description = x['definition'],
                          obj_url = '/so/' + x['id'],
-                         is_obsolete = '0',
+                         is_obsolete = 'false',
                          created_by = CREATED_BY)
             nex_session.add(this_x)
             nex_session.flush()
@@ -168,7 +168,7 @@ def load_new_data(nex_session, data, source_to_id, soid_to_so, ro_id, so_id_to_a
             continue
         to_delete.append((soid, x.display_name))
         if x.is_obsolete is False:
-            x.is_obsolete = '1'
+            x.is_obsolete = 'true'
             nex_session.add(x)
             nex_session.flush()
             update_log['updated'] = update_log['updated'] + 1
