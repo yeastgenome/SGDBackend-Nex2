77abce2553248c720a9173dead4081dc3c86e075
sweng66 <sweng@stanford.edu>
2022-02-07 11:12:14 -0800

    testing

--- scripts/loading/ontology/disease.py
+++ scripts/loading/ontology/disease.py
@@ -112,7 +112,7 @@ def load_new_data(nex_session, data, source_to_id, doid_to_disease, ro_id, disea
             disease_id = y.disease_id
 
             if y.is_obsolete is True:
-                y.is_obsolete = '0'
+                y.is_obsolete = 'false'
                 nex_session.add(y)
                 nex_session.flush()
                 update_log['updated'] = update_log['updated'] + 1
@@ -142,7 +142,7 @@ def load_new_data(nex_session, data, source_to_id, doid_to_disease, ro_id, disea
                              display_name = x['term'],
                              description = x['definition'],
                              obj_url = '/disease/' + x['id'],
-                             is_obsolete = '0',
+                             is_obsolete = 'false',
                              created_by = CREATED_BY)
             nex_session.add(this_x)
             nex_session.flush()
@@ -202,7 +202,7 @@ def load_new_data(nex_session, data, source_to_id, doid_to_disease, ro_id, disea
             continue
         to_delete.append((doid, x.display_name))
         if x.is_obsolete is False:
-            x.is_obsolete = '1'
+            x.is_obsolete = 'true'
             nex_session.add(x)
             nex_session.flush()
             update_log['updated'] = update_log['updated'] + 1
