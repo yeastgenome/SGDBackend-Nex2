36c0e7ea40abf221bdac42966c6feda08fd9d0b9
sweng66 <sweng@stanford.edu>
2022-02-07 11:13:58 -0800

    testing

--- scripts/loading/ontology/ec.py
+++ scripts/loading/ontology/ec.py
@@ -80,7 +80,7 @@ def load_new_data(nex_session, data, source_to_id, ecid_to_ec, ec_id_to_alias, f
             y = ecid_to_ec[id]
             ec_id = y.ec_id
             if y.is_obsolete is True:
-                y.is_obsolete = '0'
+                y.is_obsolete = 'false'
                 nex_session.add(y)
                 nex_session.flush()
                 update_log['updated'] = update_log['updated'] + 1
@@ -103,7 +103,7 @@ def load_new_data(nex_session, data, source_to_id, ecid_to_ec, ec_id_to_alias, f
                         ecid = id,
                         description = x['description'],
                         obj_url = '/ecnumber/' + id,
-                        is_obsolete = '0',
+                        is_obsolete = 'false',
                         created_by = CREATED_BY)
             nex_session.add(this_x)
             nex_session.flush()
@@ -138,7 +138,7 @@ def load_new_data(nex_session, data, source_to_id, ecid_to_ec, ec_id_to_alias, f
             continue
         to_delete.append(ecid)
         if x.is_obsolete is False:
-            x.is_obsolete = '1'
+            x.is_obsolete = 'true'
             nex_session.add(x)
             nex_session.flush()
             update_log['updated'] = update_log['updated'] + 1
