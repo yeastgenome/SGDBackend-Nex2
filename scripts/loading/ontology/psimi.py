c3a5bb04efa9ab3e7edaefd089f583152058b54a
sweng66 <sweng@stanford.edu>
2022-02-07 11:23:07 -0800

    testing

--- scripts/loading/ontology/psimi.py
+++ scripts/loading/ontology/psimi.py
@@ -110,7 +110,7 @@ def load_new_data(nex_session, data, source_to_id, psimiid_to_psimi, ro_id, roid
             y = psimiid_to_psimi[x['id']]
             psimi_id = y.psimi_id
             if y.is_obsolete is True:
-                y.is_obsolete = '0'
+                y.is_obsolete = 'false'
                 nex_session.add(y)
                 nex_session.flush()
                 update_log['updated'] = update_log['updated'] + 1
@@ -134,7 +134,7 @@ def load_new_data(nex_session, data, source_to_id, psimiid_to_psimi, ro_id, roid
                          display_name = x['term'],
                          description = x['definition'],
                          obj_url = '/psimi/' + x['id'],
-                         is_obsolete = '0',
+                         is_obsolete = 'false',
                          created_by = CREATED_BY)
             nex_session.add(this_x)
             nex_session.flush()
@@ -203,7 +203,7 @@ def load_new_data(nex_session, data, source_to_id, psimiid_to_psimi, ro_id, roid
             continue
         to_delete.append((psimiid, x.display_name))
         if x.is_obsolete is False:
-            x.is_obsolete = '1'
+            x.is_obsolete = 'true'
             nex_session.add(x)
             nex_session.flush()
             update_log['updated'] = update_log['updated'] + 1
