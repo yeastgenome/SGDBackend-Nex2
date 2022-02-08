42fe175e0ab2fb82b3033b5979942d05110a8057
sweng66 <sweng@stanford.edu>
2022-02-07 19:59:47 -0800

    fixed a few bugs

--- scripts/loading/ontology/taxonomy.py
+++ scripts/loading/ontology/taxonomy.py
@@ -183,7 +183,7 @@ def load_new_data(nex_session, data, source_to_id, taxid_to_taxonomy, ro_id, tax
             nex_session.add(x)
             nex_session.flush()
             update_log['updated'] = update_log['updated'] + 1
-            fw.write("The is_obsolete for " + x.taxid + " has been updated from " + x.is_obsolete +" to " + 'True' + "\n")
+            fw.write("The is_obsolete for " + x.taxid + " has been updated from " + str(x.is_obsolete) +" to " + 'True' + "\n")
 
     nex_session.commit()
  
