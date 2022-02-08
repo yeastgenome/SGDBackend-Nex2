69de532e27c564f15ab43a056cfd272a9c795f25
sweng66 <sweng@stanford.edu>
2022-02-07 20:28:28 -0800

    fixed bool values to True/False

--- scripts/loading/go/load_gpad.py
+++ scripts/loading/go/load_gpad.py
@@ -659,9 +659,9 @@ def update_database_load_file_to_s3(nex_session, go_file, source_to_id, edam_to_
                     format_id=format_id,
                     topic_id=topic_id,
                     status='Active',
-                    is_public='true',
-                    is_in_spell='false',
-                    is_in_browser='false',
+                    is_public=True,
+                    is_in_spell=False,
+                    is_in_browser=False,
                     file_date=datetime.now(),
                     source_id=source_to_id['SGD'],
                     md5sum=go_md5sum)
