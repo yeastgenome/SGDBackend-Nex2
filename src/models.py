0407db8ec0345149e56d6553287a6e76ccdb3925
sweng66 <sweng@stanford.edu>
2022-02-25 14:18:53 -0800

    testing

--- src/models.py
+++ src/models.py
@@ -3353,6 +3353,10 @@ class Locusdbentity(Dbentity):
             "1kb": []
         }
 
+        sequence_summary = DBSession.query(Locussummary.html).filter_by(locus_id=self.dbentity_id, summary_type="Sequence").one_or_none()
+        if sequence_summary:
+            ["paragraph"] = sequence_summary[0]
+        
         main_strain_genomic = None
         main_strain_coding = None
         main_strain_1KB = None
@@ -4926,6 +4930,10 @@ class Locusdbentity(Dbentity):
             "median_abs_dev_value": None
         }
 
+        protein_summary = DBSession.query(Locussummary.html).filter_by(locus_id=self.dbentity_id, summary_type="Protein").one_or_none()
+        if protein_summary:
+            ["paragraph"] = protein_summary[0]
+        
         taxonomy_id = self.get_main_strain('taxonomy_id')
         protein = DBSession.query(Proteinsequenceannotation).filter_by(dbentity_id=self.dbentity_id, taxonomy_id=taxonomy_id).one_or_none()
         if protein:
