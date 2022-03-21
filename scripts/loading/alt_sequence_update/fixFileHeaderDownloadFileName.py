4f84fae55f33f2c5ee7911191e6075bbd11f3aea
sweng66 <sweng@stanford.edu>
2022-03-21 16:11:44 -0700

    adding code to update proteinsequenceannotation table

--- scripts/loading/alt_sequence_update/fixFileHeaderDownloadFileName.py
+++ scripts/loading/alt_sequence_update/fixFileHeaderDownloadFileName.py
@@ -1,6 +1,6 @@
 from sqlalchemy import or_
 from datetime import datetime
-from src.models import Locusdbentity, Dnasequenceannotation
+from src.models import Locusdbentity, Dnasequenceannotation, Proteinsequenceannotation
 from scripts.loading.database_session import get_session
 
 __author__ = 'sweng66'
@@ -13,7 +13,9 @@ nex_session = get_session()
 
 dbentity_id_to_names = dict([(x.dbentity_id, (x.systematic_name, x.gene_name)) for x in nex_session.query(Locusdbentity).all()])
 
+## switch to update either one of the tables
 all_rows = nex_session.query(Dnasequenceannotation).all()
+# all_rows = nex_session.query(Proteinsequenceannotation).all()
 
 i = 0
 j = 0
