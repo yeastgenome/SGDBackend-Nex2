229b6e8d4db756350e29b5539dc27432fae3bc2a
sweng66 <sweng@stanford.edu>
2022-03-24 12:41:46 -0700

    minor fix

--- scripts/loading/alt_sequence_update/load_seq.py
+++ scripts/loading/alt_sequence_update/load_seq.py
@@ -1,6 +1,7 @@
 from sqlalchemy import or_
 from datetime import datetime
-from src.models import Dnasequenceannotation
+import os
+from src.models import Dnasequenceannotation, Source
 from scripts.loading.database_session import get_session
 
 __author__ = 'sweng66'
@@ -9,13 +10,19 @@ infile = "scripts/loading/alt_sequence_update/data/altRefGenomeUpdate031822_with
 
 genomic_dna_type = 'GENOMIC'
 oneKB_dna_type = '1KB'
-        
+created_by = os.environ['DEFAULT_USER']
+
 def load_seq():
     
     f = open(infile)
 
+    print (datetime.now())
+    
     nex_session = get_session()
 
+    source = nex_session.query(Source).filter_by(display_name='SGD').one_or_none()
+    source_id = source.source_id
+    
     key_to_annotation_id = dict([((x.dna_type, x.dbentity_id, x.so_id, x.contig_id, x.taxonomy_id), x.annotation_id) for x in nex_session.query(Dnasequenceannotation).all()])
         
     i = 0
@@ -47,39 +54,44 @@ def load_seq():
         oneKB_key = (oneKB_dna_type, dbentity_id, so_id, contig_id, taxonomy_id)
 
         if genomic_key not in key_to_annotation_id:
-            insert_dnasequenceannotation(nex_session, genomic_dna_type, dbentity_id, so_id,
-                                         contig_id, taxonomy_id, strand, genomic_start_index,
+            insert_dnasequenceannotation(nex_session, genomic_dna_type, source_id, dbentity_id,
+                                         so_id, contig_id, taxonomy_id, strand, genomic_start_index,
                                          genomic_end_index, genomic_file_header,
                                          genomic_download_filename, genomic_seq)
             i = i + 1
             j = j + 1
-            
+
+            print (j, systematic_name, genomic_dna_type)
+                
         if oneKB_key not in key_to_annotation_id:
-            insert_dnasequenceannotation(nex_session, oneKB_dna_type, dbentity_id, so_id,
-                                         contig_id, taxonomy_id, strand, oneKB_start_index,
+            insert_dnasequenceannotation(nex_session, oneKB_dna_type, source_id, dbentity_id,
+                                         so_id, contig_id, taxonomy_id, strand, oneKB_start_index,
                                          oneKB_end_index, oneKB_file_header,
                                          oneKB_download_filename, oneKB_seq)
             i =	i + 1
             j =	j + 1
 
+            print (j, systematic_name, oneKB_dna_type)
+            
         if i > 300:
             i = 0
-            nex_session.rollback()
-            # nex_session.commit()
+            # nex_session.rollback()
+            nex_session.commit()
             
     f.close()
     
-    nex_session.rollback()
-    # nex_session.commit()
-
+    # nex_session.rollback()
+    nex_session.commit()
     nex_session.close()
+    print (datetime.now())
     
 
-def insert_dnasequenceannotation(nex_session, dna_type, dbentity_id, so_id, contig_id, taxonomy_id, strand, start_index, end_index, file_header, download_filename, seq):
+def insert_dnasequenceannotation(nex_session, dna_type, source_id, dbentity_id, so_id, contig_id, taxonomy_id, strand, start_index, end_index, file_header, download_filename, seq):
 
     x = Dnasequenceannotation(dna_type = dna_type,
                               dbentity_id = dbentity_id,
                               so_id = so_id,
+                              source_id = source_id,
                               contig_id = contig_id,
                               taxonomy_id = taxonomy_id,
                               strand = strand,
@@ -87,7 +99,8 @@ def insert_dnasequenceannotation(nex_session, dna_type, dbentity_id, so_id, cont
                               end_index = end_index,
                               file_header = file_header,
                               download_filename = download_filename,
-                              residues = seq)
+                              residues = seq,
+                              created_by = created_by)
 
     nex_session.add(x)
         
