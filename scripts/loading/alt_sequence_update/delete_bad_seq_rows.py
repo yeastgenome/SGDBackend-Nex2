85eb23a2bc25fc7ff44b9af10b515d0f8b795289
sweng66 <sweng@stanford.edu>
2022-05-25 14:32:04 -0700

    fixed the program to delete rows from proteinsequece_detail table

--- scripts/loading/alt_sequence_update/delete_bad_seq_rows.py
+++ scripts/loading/alt_sequence_update/delete_bad_seq_rows.py
@@ -1,7 +1,8 @@
 from sqlalchemy import or_
 from datetime import datetime
 import os
-from src.models import Dnasubsequence, Dnasequenceannotation, Proteinsequenceannotation
+from src.models import Dnasubsequence, Dnasequenceannotation, Proteinsequenceannotation,\
+    ProteinsequenceDetail
 from scripts.loading.database_session import get_session
 
 __author__ = 'sweng66'
@@ -28,26 +29,36 @@ for id in bad_ids:
 
     for x in nex_session.query(Dnasubsequence).filter_by(annotation_id = int(id)).all():
         nex_session.delete(x)
+        print ("DELETE Dnasubsequence row for annotation_id = " + id)
         
     dbentity_id = None
     taxonomy_id = None
     file_header = None
-    for x in nex_session.query(Dnasequenceannotation).filter_by(annotation_id = int(id)).all(): 
-        if x.dna_type == 'GENOMIC':
-            dbentity_id = x.dbentity_id
-            taxonomy_id = x.taxonomy_id
-            header = x.file_header.split(' ')
-            file_header = ' '.join(header[0:2]) + ' ' + ' '.join(header[3:])
-        nex_session.delete(x)
-
-    if dbentity_id is None:
-        continue
-    
-    x = nex_session.query(Proteinsequenceannotation).filter_by(dbentity_id = dbentity_id, taxonomy_id = taxonomy_id, file_header = file_header).one_or_none()
+    x = nex_session.query(Dnasequenceannotation).filter_by(annotation_id = int(id)).one_or_none()
     if x is None:
         continue
-    print (y.annotation_id, dbentity_id, taxonomy_id, file_header)
+    if x.dna_type == 'GENOMIC':
+        dbentity_id = x.dbentity_id
+        taxonomy_id = x.taxonomy_id
+        header = x.file_header.split(' ')
+        file_header = ' '.join(header[0:2]) + ' ' + ' '.join(header[3:])
     nex_session.delete(x)
+    print ("DELETE Dnasequenceannotation row for annotation_id = " + id)
+    
+    if dbentity_id is None:
+        continue
+
+    x = nex_session.query(Proteinsequenceannotation).filter_by(dbentity_id = dbentity_id, taxonomy_id = taxonomy_id, file_header = file_header).one_or_none()        
+    if x is None:
+        continue
+    
+    y =	nex_session.query(ProteinsequenceDetail).filter_by(annotation_id = x.annotation_id).one_or_none()
+    if y:
+        nex_session.delete(y)
+        print ("DELETE ProteinsequenceDetail row for annotation_id = " + str(x.annotation_id) + ", dbentity_id = " + str(dbentity_id) + ", taxonomy_id = " + str(taxonomy_id) + ", file_header = " + file_header)
+        
+    nex_session.delete(x)
+    print ("DELETE Proteinsequenceannotation row for annotation_id = " + str(x.annotation_id) + ", dbentity_id = " + str(dbentity_id) + ", taxonomy_id = " + str(taxonomy_id) + ", file_header = " + file_header)
     
 nex_session.rollback()
 # nex_session.commit()
