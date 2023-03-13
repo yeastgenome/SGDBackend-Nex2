d0a5e81a4bb77327840ff5d01b0dbe0b876617b0
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-03-13 09:15:03 -0700

    Add job timestamps to output

--- system_config/cron/index_elasticsearch.sh
+++ system_config/cron/index_elasticsearch.sh
@@ -2,5 +2,9 @@
 
 cd /data/www/SGDBackend-Nex2
 
+echo "index_elasticsearch.sh start:  `/bin/date`"
+
 # elasticsearch 7 script
 . venv/bin/activate && python scripts/search/index_es_7.py
+
+echo "index_elasticsearch.sh end:  `/bin/date`"
