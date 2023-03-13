8eaa54d500a5d3984559860c910207e357e6a45e
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-03-13 09:14:00 -0700

    Add job timestamps to output

--- system_config/cron/index_disambiguation.sh
+++ system_config/cron/index_disambiguation.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
+echo "index_disambiguation.sh start:  `/bin/date`"
+
 cd /data/www/SGDBackend-Nex2
 . venv/bin/activate && python scripts/disambiguation/index_disambiguation.py
+
+echo "index_disambiguation.sh end:  `/bin/date`"
