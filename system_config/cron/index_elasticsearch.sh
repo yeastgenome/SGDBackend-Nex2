3741adbedbc6af11540c89b3b95b0be7b750d89c
sweng66 <sweng@stanford.edu>
2023-03-14 09:30:42 -0700

    Master docker cron (#420)
    * Update to run under Fargate
    
    "source" doesn't work under Fargate.  Also, environment variables are set in a file in S3, so prod_variables.sh is no longer needed.
    
    * Update to run under Fargate
    
    "source" doesn't work under Fargate.  Also, environment variables are set in a file in S3, so prod_variables.sh is no longer needed.
    
    * Add job timestamps to output
    
    * Add job timestamps to output
    
    ---------
    
    Co-authored-by: stuartmiyasato <stuartmiyasato@users.noreply.github.com>

--- system_config/cron/index_elasticsearch.sh
+++ system_config/cron/index_elasticsearch.sh
@@ -4,6 +4,8 @@ echo "index_elasticsearch.sh start:  `/bin/date`"
 
 cd /data/www/SGDBackend-Nex2
 
+echo "index_elasticsearch.sh start:  `/bin/date`"
+
 # elasticsearch 7 script
 . venv/bin/activate && python scripts/search/index_es_7.py
 
