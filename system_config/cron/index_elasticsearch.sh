a8a04fa396ee82f74889bbe1ac61efa7097e17b2
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-03-15 14:57:39 -0700

    Send output to logs and via email

--- system_config/cron/index_elasticsearch.sh
+++ system_config/cron/index_elasticsearch.sh
@@ -1,12 +1,15 @@
-#! /bin/sh
+#!/bin/sh
 
-echo "index_elasticsearch.sh start:  `/bin/date`"
+OUTPUT_FILE=/tmp/index_elasticsearch.out
+
+echo "index_elasticsearch.sh start:  `/bin/date`" | /bin/tee $OUTPUt_FILE
 
 cd /data/www/SGDBackend-Nex2
 
-echo "index_elasticsearch.sh start:  `/bin/date`"
-
 # elasticsearch 7 script
-. venv/bin/activate && python scripts/search/index_es_7.py
+. venv/bin/activate && \
+    python scripts/search/index_es_7.py | /bin/tee -a $OUTPUt_FILE
 
-echo "index_elasticsearch.sh end:  `/bin/date`"
+echo "index_elasticsearch.sh end:  `/bin/date`" | /bin/tee -a $OUTPUt_FILE
+
+/bin/mail -s "index_elasticsearch.sh: `/bin/date`" $CRON_EMAIL < $OUTPUT_FILE
