734cc19ea3217613b5cb7adad1eae117ff76a6c4
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-03-21 21:38:35 -0700

    Create topic in the correct region

--- system_config/cron/index_disambiguation.sh
+++ system_config/cron/index_disambiguation.sh
@@ -11,6 +11,6 @@ cd /data/www/SGDBackend-Nex2
 echo "index_disambiguation.sh end:  `/bin/date`" | /bin/tee -a $OUTPUT_FILE
 
 /usr/local/bin/aws sns publish \
-    --topic-arn "arn:aws:sns:us-east-1:172390527433:cron_jobs_qa" \
+    --topic-arn "arn:aws:sns:us-west-2:172390527433:cron_jobs_qa" \
     --region us-west-2 \
     --message file://${OUTPUT_FILE}
