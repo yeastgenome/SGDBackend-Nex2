817b84102c7a16ebf3ab8f2afffb69b86410102f
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-03-22 14:42:25 -0700

    Fix another typo

--- system_config/cron/index_elasticsearch.sh
+++ system_config/cron/index_elasticsearch.sh
@@ -2,15 +2,15 @@
 
 OUTPUT_FILE=/tmp/index_elasticsearch.out
 
-echo "index_elasticsearch.sh start:  `/bin/date`" | /bin/tee $OUTPUt_FILE
+echo "index_elasticsearch.sh start:  `/bin/date`" | /bin/tee $OUTPUT_FILE
 
 cd /data/www/SGDBackend-Nex2
 
 # elasticsearch 7 script
 . venv/bin/activate && \
-    python scripts/search/index_es_7.py | /bin/tee -a $OUTPUt_FILE
+    python scripts/search/index_es_7.py | /bin/tee -a $OUTPUT_FILE
 
-echo "index_elasticsearch.sh end:  `/bin/date`" | /bin/tee -a $OUTPUt_FILE
+echo "index_elasticsearch.sh end:  `/bin/date`" | /bin/tee -a $OUTPUT_FILE
 
 /usr/local/bin/aws sns publish \
     --topic-arn $SNS_TOPIC_ARN \
