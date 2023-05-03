6f9c4d1d056398bb19b1eae7afaad725438e3268
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-05-03 15:15:42 -0700

    Changes for Fargate
    Add begin and end timestamps, and send SNS notification when job completes.

--- system_config/cron/data_dump.sh
+++ system_config/cron/data_dump.sh
@@ -1,7 +1,17 @@
-#! /bin/sh
+#!/bin/sh
+
+OUTPUT_FILE=/tmp/data_dump.out
+
+echo "data_dump.sh start:  `/bin/date`" | /bin/tee $OUTPUT_FILE
 
 cd /data/www/SGDBackend-Nex2
-source /data/www/venv/bin/activate 
-source prod_variables.sh 
-python scripts/dumping/curation/dump_gff.py
+. venv/bin/activate && \
+    python scripts/dumping/curation/dump_gff.py | /bin/tee -a $OUTPUT_FILE
 
+echo "data_dump.sh end:  `/bin/date`" | /bin/tee -a $OUTPUT_FILE
+
+/usr/bin/aws sns publish \
+    --topic-arn $SNS_TOPIC_ARN \
+    --subject "dump_gff.py completed" \
+    --message file://${OUTPUT_FILE} \
+    --region $AWS_REGION
