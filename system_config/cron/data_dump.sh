6e6e3daf1f7cfe1a980655c193f76de52621af25
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-05-15 16:00:54 -0700

    Use v2 of AWS CLI

--- system_config/cron/data_dump.sh
+++ system_config/cron/data_dump.sh
@@ -15,4 +15,4 @@ echo "data_dump.sh end:  `/bin/date`" | /bin/tee -a $OUTPUT_FILE
 
 echo '{"Data": "From: '$(echo $EMAIL_FROM)'\nTo: '$(echo $EMAIL_TO)'\nSubject: data_dump.sh report\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary=\"NextPart\"\n\n--NextPart\nContent-Type: text/plain\n\ndata_dump.sh completed successfully\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename=\"data_dump_report.txt\"\n\n'$(cat $OUTPUT_FILE)'\n--NextPart--"}' > $MESSAGE_JSON_FILE
 
-/usr/bin/aws ses send-raw-email --cli-binary-format raw-in-base64-out --raw-message file://${MESSAGE_JSON_FILE}
+/usr/local/bin/aws ses send-raw-email --cli-binary-format raw-in-base64-out --raw-message file://${MESSAGE_JSON_FILE}
