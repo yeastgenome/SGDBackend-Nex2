918ccfa7abdd0159e47a3d7b240de70249a268c3
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-05-15 11:57:40 -0700

    Send report as email attachment

--- system_config/cron/data_dump.sh
+++ system_config/cron/data_dump.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 OUTPUT_FILE=${LOG_FILE}
+MESSAGE_JSON_FILE="/tmp/message.json"
 
 echo "data_dump.sh start:  `/bin/date`" | /bin/tee $OUTPUT_FILE
 
@@ -10,9 +11,8 @@ cd /data/www/SGDBackend-Nex2
 
 echo "data_dump.sh end:  `/bin/date`" | /bin/tee -a $OUTPUT_FILE
 
-/usr/bin/aws ses send-email \
-    --destination "ToAddresses=${EMAIL_TO}" \
-    --from $EMAIL_FROM \
-    --subject "data_dump.sh completed" \
-    --text file://${OUTPUT_FILE}
-    
+/usr/bin/sed -i 's/$/\\n/' $OUTPUT_FILE
+
+echo '{"Data": "From: '$(echo $EMAIL_FROM)'\nTo: '$(echo $EMAIL_TO)\nSubject: data_dump.sh report\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary=\"NextPart\"\n\n--NextPart\nContent-Type: text/plain\n\ndata_dump.sh completed successfully\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename=\"data_dump_report.txt\"\n\n'$(cat $OUTPUT_FILE)'\n--NextPart--"}' > /tmp/message.json
+
+/usr/bin/aws ses send-raw-email --cli-binary-format raw-in-base64-out --raw-message file://${MESSAGE_JSON_FILE}
