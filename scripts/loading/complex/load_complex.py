95526787d64fafa386815df8c478c1b7643314b5
sweng66 <sweng@stanford.edu>
2023-06-06 19:54:40 -0700

    fixed the URL to retrieve complex data from EBI

--- scripts/loading/complex/load_complex.py
+++ scripts/loading/complex/load_complex.py
@@ -9,7 +9,8 @@ import importlib
 importlib.reload(sys)  # Reload does the trick!
 from src.models import Source, Psimi, Dbentity, Go, Taxonomy, Eco, Referencedbentity, \
        Locusdbentity, Complexdbentity, ComplexAlias, ComplexGo, ComplexReference, \
-       Interactor, LocusAlias, Complexbindingannotation, Literatureannotation
+       Interactor, LocusAlias, Complexbindingannotation, Literatureannotation, \
+       Goannotation, Goslimannotation
 from scripts.loading.database_session import get_session
 from scripts.loading.reference.promote_reference_triage import add_paper
 
@@ -26,7 +27,9 @@ log.setLevel(logging.INFO)
 
 CREATED_BY = os.environ['DEFAULT_USER']
 
-all_json_url = "https://www.ebi.ac.uk/intact/complex-ws/search/*?format=json&facets=species_f&filters=species_f:(%22Saccharomyces%20cerevisiae%20\(strain%20ATCC%20204508%20/%20S288c\)%22)"
+# all_json_url = "https://www.ebi.ac.uk/intact/complex-ws/search/*?format=json&facets=species_f&filters=species_f:(%22Saccharomyces%20cerevisiae%20\(strain%20ATCC%20204508%20/%20S288c\)%22)"
+
+all_json_url = "https://www.ebi.ac.uk/intact/complex-ws/search/*?filters=species_f:(%22Saccharomyces%20cerevisiae%20(strain%20ATCC%20204508%20/%20S288c)%22)&facets=species_f"
 
 detail_json_url_template = "https://www.ebi.ac.uk/intact/complex-ws/complex/REPLACE_ID_HERE"
 
@@ -291,7 +294,11 @@ def delete_complex(nex_session, complexAC):
     ## delete go
     for	x in nex_session.query(ComplexGo).filter_by(complex_id=complex.dbentity_id).all():
         nex_session.delete(x)
-
+    for x in nex_session.query(Goannotation).filter_by(dbentity_id=complex.dbentity_id).all():
+        nex_session.delete(x)
+    for x in nex_session.query(Goslimannotation).filter_by(dbentity_id=complex.dbentity_id).all():
+        nex_session.delete(x)
+        
     ## delete reference
     for x in nex_session.query(ComplexReference).filter_by(complex_id=complex.dbentity_id).all():
         nex_session.delete(x)
