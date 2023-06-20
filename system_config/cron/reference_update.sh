8f415f6fb2088017185bb67650feb8b145a4fe96
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-06-19 19:41:12 -0700

    Send log output to email

--- system_config/cron/reference_update.sh
+++ system_config/cron/reference_update.sh
@@ -1,9 +1,24 @@
-#! /bin/sh
+#! /bin/sh -x
 
+OUTPUT_FILE="/tmp/output.log"
+LOG_FILE="scripts/loading/reference/logs/reference_update.log"
+MESSAGE_JSON_FILE="/tmp/message.json"
 
 cd /data/www/SGDBackend-Nex2
-source /data/www/venv/bin/activate 
-source prod_variables.sh 
-python scripts/loading/reference/reference_update_from_abc.py
-python scripts/loading/reference/reference_display_name_update.py
-python scripts/dumping/ncbi/dump_gene_pmid_pair.py
+. /data/www/venv/bin/activate 
+
+/usr/bin/touch $OUTPUT_FILE
+
+python scripts/loading/reference/reference_update_from_abc.py | /usr/bin/tee -a $OUTPUT_FILE
+python scripts/loading/reference/reference_display_name_update.py | /usr/bin/tee -a $OUTPUT_FILE
+python scripts/dumping/ncbi/dump_gene_pmid_pair.py | /usr/bin/tee -a $OUTPUT_FILE
+
+cat $LOG_FILE >> $OUTPUT_FILE
+
+/usr/bin/sed -i 's/$/\\n/' $OUTPUT_FILE
+
+echo '{"Data": "From: '$(echo $EMAIL_FROM)'\nTo: '$(echo $EMAIL_TO)'\nSubject: data_dump.sh report\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary=\"NextPart\"\n\n--NextPart\nContent-Type: text/plain\n\ndata_dump.sh completed successfully\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; filename=\"data_dump_report.txt\"\n\n'$(cat $OUTPUT_FILE)'\n--NextPart--"}' > $MESSAGE_JSON_FILE
+
+/usr/local/bin/aws ses send-raw-email --cli-binary-format raw-in-base64-out --raw-message file://${MESSAGE_JSON_FILE}
+
+exit 0
