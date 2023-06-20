db03b61740afb9359d3b458d34c9aab987a621c4
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-06-20 10:31:54 -0700

    Remove unneeded spaces

--- scripts/loading/reference/reference_update_from_abc.py
+++ scripts/loading/reference/reference_update_from_abc.py
@@ -181,7 +181,7 @@ def download_reference_json_file_from_alliance_s3():
     s3_client = boto3.client('s3',
                              aws_access_key_id=environ['ABC_AWS_ACCESS_KEY_ID'],
                              aws_secret_access_key=environ['ABC_AWS_SECRET_ACCESS_KEY'],
-                             region_name = environ['ABC_BUCKET_REGION'])
+                             region_name=environ['ABC_BUCKET_REGION'])
     try:
         response = s3_client.download_file(bucketname, s3_file_location, json_file)
         if response is not None:
