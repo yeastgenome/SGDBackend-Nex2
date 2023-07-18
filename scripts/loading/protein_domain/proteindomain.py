a55ff949881c97e4645e92fc605f22ae2e0a44b7
sweng66 <sweng@stanford.edu>
2023-07-18 13:21:39 -0700

    fixed the domain loading programs to work new domain types

--- scripts/loading/protein_domain/proteindomain.py
+++ scripts/loading/protein_domain/proteindomain.py
@@ -1,6 +1,7 @@
-import os
+from datetime import datetime
 import sys
-from src.models import Source, Proteindomain, ProteindomainUrl, Proteindomainannotation
+import os
+from src.models import Source, Proteindomain, ProteindomainUrl
 from scripts.loading.database_session import get_session
                  
 __author__ = 'sweng66'
@@ -8,11 +9,10 @@ __author__ = 'sweng66'
 ## Created on June 2017
 ## This script is used to update protein domains in NEX2.
 
-# domain_file = 'scripts/loading/protein_domain/data/orf_trans_all.fasta_full.tvs'
-domain_file = 'scripts/loading/protein_domain/data/unique_domain_data.txt'
-log_file = 'scripts/loading/protein_domain/logs/protein_domain.log'
-
 CREATED_BY = os.environ['DEFAULT_USER']
+
+domain_file = 'scripts/loading/protein_domain/data/orf_trans_all.fasta.tsv'
+log_file = 'scripts/loading/protein_domain/logs/protein_domain.log'
 
 def load_domains():
 
@@ -34,77 +34,86 @@ def read_data_and_update_database(nex_session, fw):
 
     f = open(domain_file)
 
-    inNew = {}
+    found = {}
     for line in f:
         items = line.strip().split("\t")
-        format_name = items[0]
-        display_name = items[1]
-        source = items[2]
-        interpro_id = items[3]
-        desc = items[4]                
-        source_id = source_to_id.get(source)
-        if source_id is None:
-            print("SOURCE:", source, " is not in the database.")
+        display_name = items[4]
+        format_name = display_name.replace(' ', '_')
+        if format_name in found:
             continue
+        found[format_name] = 1
 
-        inNew[format_name] = 1
-        
+        source = items[3]
+        if source in ['ProSiteProfiles', 'ProSitePatterns']:
+            source = 'PROSITE'
+        if source in ['FunFam', 'GENE3D']:
+            source = 'Gene3D'
+        if source == 'NCBIfam':
+            source = 'CDD'
+        if source == 'Hamap':
+            source = 'HAMAP'
+        source_id = source_to_id.get(source)
+        # if source_id is None:
+        if source_id is None:
+            print ("SOURCE:", source, " is not in the database.")
+            continue
+            
+        interpro_id = ""
+        desc = ""
+        if len(items) > 11:
+            interpro_id = items[11]
+        if len(items) > 12:
+            desc = items[12]
+            
         x = format_name_to_domain.get(format_name)
-
+        
         if x is None:
+
+            print ("not in DB:", format_name, display_name, source, source_id)
+
             proteindomain_id = insert_new_domain(nex_session, fw, format_name,
                                                  display_name, source_id, 
                                                  interpro_id, desc)
-            insert_url(nex_session, fw, display_name, source, proteindomain_id, 
-                       source_id)
-            nex_session.commit()
-        else:            
-            if x.interpro_id != interpro_id or x.description != desc:
-                x.interpro_id = interpro_id
-                x.description = desc
-                nex_session.add(x)
-                nex_session.commit()
-                fw.write("Update: " + format_name + "\n")
+            if proteindomain_id:
+                status = insert_url(nex_session, fw, display_name, source,  proteindomain_id, 
+                                    source_id)
+                if status:
+                    nex_session.commit()
+                else:
+                    nex_session.rollback()
+            continue
+        else:
+            print ("in DB:", format_name, display_name, source, source_id)
 
+        if x.interpro_id != interpro_id or x.description != desc:
+            x.interpro_id = interpro_id
+            x.description = desc
+            nex_session.add(x)
+            nex_session.commit()
+            fw.write("Update domain: " + display_name + "\n")
                 
     f.close()
 
-    ## comment out so it won't delete the domains for other strains
-    # delete_old_domains(nex_session, fw, format_name_to_domain, inNew)
-
-
-def delete_old_domains(nex_session, fw, format_name_to_domain, inNew):
-
-    for key in format_name_to_domain:
-        if key in inNew:
-            continue
-        x = format_name_to_domain[key]
-        nex_session.query(ProteindomainUrl).filter_by(proteindomain_id=x.proteindomain_id).delete()
-        nex_session.query(Proteindomainannotation).filter_by(proteindomain_id=x.proteindomain_id).delete()
-        nex_session.delete(x)
-        nex_session.commit()
-
-        fw.write("Delete domain: " + key + "\n")
-
-
 def insert_new_domain(nex_session, fw, format_name, display_name, source_id, interpro_id, desc):
 
-    x = Proteindomain(display_name = display_name,
-                      format_name = format_name,
-                      source_id = source_id,
-                      obj_url = '/domain/' + display_name.replace(' ', '_'),
-                      interpro_id = interpro_id,
-                      description = desc,
-                      created_by = CREATED_BY)
-
-    nex_session.add(x)
-    nex_session.flush()
-    nex_session.refresh(x)
-
-    fw.write("Insert new domain: " + display_name + "\n")
-
-    return x.proteindomain_id
-
+    proteindomain_id = None
+    try:
+        x = Proteindomain(display_name = display_name,
+                          format_name = format_name,
+                          source_id = source_id,
+                          obj_url = '/domain/' + display_name.replace(' ', '_'),
+                          interpro_id = interpro_id,
+                          description = desc,
+                          created_by = CREATED_BY)
+        nex_session.add(x)
+        nex_session.flush()
+        nex_session.refresh(x)
+        fw.write("Insert new domain: " + display_name + "\n")
+        proteindomain_id = x.proteindomain_id
+    except Exception as e:
+        fw.write("Error inserting new domain: " + display_name + ". error = " + str(e) + "\n")
+        
+    return proteindomain_id
 
 def insert_url(nex_session, fw, display_name, source, proteindomain_id, source_id):
     
@@ -113,8 +122,9 @@ def insert_url(nex_session, fw, display_name, source, proteindomain_id, source_i
         link = "http://smart.embl-heidelberg.de/smart/do_annotation.pl?DOMAIN=" + display_name
     elif source == 'Pfam':
         link = "http://pfam.xfam.org/family/" + display_name
-    elif source == 'GENE3D' or source == 'Gene3D':
+    elif source in ['GENE3D', 'Gene3D', 'FunFam']:
         source = 'GENE3D'
+        linkID = display_name[6:].split(':FF:')[0]
         link = "http://www.cathdb.info/version/latest/superfamily/" + display_name[6:]
     elif source == 'SUPERFAMILY':
         link = "http://supfam.org/SUPERFAMILY/cgi-bin/scop.cgi?ipid=" + display_name
@@ -128,21 +138,27 @@ def insert_url(nex_session, fw, display_name, source, proteindomain_id, source_i
         link = "http://prodom.prabi.fr/prodom/current/cgi-bin/request.pl?question=DBEN&amp;query=" + display_name
     elif source == 'PIRSF':
         link = "http://pir.georgetown.edu/cgi-bin/ipcSF?id=" + display_name
-    elif source == 'PROSITE':
+    elif source in ['PROSITE', 'ProSiteProfiles', 'ProSitePatterns']:
         link = "http://prosite.expasy.org/cgi-bin/prosite/nicesite.pl?" + display_name 
     elif source == 'HAMAP':
         link = "http://hamap.expasy.org/profile/" + display_name
+    elif source == 'CDD':
+        link = "https://www.ncbi.nlm.nih.gov/Structure/cdd/" + display_name
     if link is not None:
-        x = ProteindomainUrl(display_name = display_name,
-                             obj_url = link,
-                             source_id = source_id,
-                             proteindomain_id = proteindomain_id,
-                             url_type = source,
-                             created_by = CREATED_BY)
-        nex_session.add(x)        
-        
-        fw.write("Add URL: " + link + " for " + display_name + "\n")
-
+        try:
+            x = ProteindomainUrl(display_name = display_name,
+                                 obj_url = link,
+                                 source_id = source_id,
+                                 proteindomain_id = proteindomain_id,
+                                 url_type = source,
+                                 created_by = CREATED_BY)
+            nex_session.add(x)        
+            fw.write("Add URL: " + link + " for " + display_name + "\n")
+            return True
+        except Exception as e:
+            fw.write("Error adding " + link + " for " + display_name + ". error=" + str(e) + "\n")
+            return False
+    
 if __name__ == "__main__":
         
     load_domains()
--- scripts/loading/protein_domain/proteindomainannotation.py
+++ scripts/loading/protein_domain/proteindomainannotation.py
@@ -1,21 +1,17 @@
+from datetime import datetime
 import os
 import sys
-from src.models import Source, Proteindomain, Locusdbentity, Taxonomy, \
-                       Proteindomainannotation
+from src.models import Source, Locusdbentity, Taxonomy, Proteindomain, Proteindomainannotation
 from scripts.loading.database_session import get_session
-                 
+
 __author__ = 'sweng66'
-
-
-domain_file = 'scripts/loading/protein_domain/data/orf_trans_all.fasta_full.tvs'
-log_file = 'scripts/loading/protein_domain/logs/protein_domain_annotation.log'
 
 CREATED_BY = os.environ['DEFAULT_USER']
 
-## This script is used to update protein domains and their annotations in NEX2 for reference genome.
+domain_file = 'scripts/loading/protein_domain/data/orf_trans_all.fasta.tsv'
+log_file = 'scripts/loading/protein_domain/logs/proteindomainannotation.log'
 
 taxid = "TAX:559292"
-today = "2020-11-18"
 
 def load_domain_annotations():
 
@@ -32,12 +28,14 @@ def load_domain_annotations():
 def read_data_and_update_database(nex_session, fw):
 
     ipr = nex_session.query(Source).filter_by(format_name='InterPro').one_or_none()
-    taxon = nex_session.query(Taxonomy).filter_by(taxid=taxid).one_or_none() 
+    taxon = nex_session.query(Taxonomy).filter_by(taxid=taxid).one_or_none()
+    taxonomy_id = taxon.taxonomy_id
+
     name_to_dbentity_id = dict([(x.systematic_name, x.dbentity_id) for x in nex_session.query(Locusdbentity).all()])
     format_name_to_id =  dict([(x.format_name, x.proteindomain_id) for x in nex_session.query(Proteindomain).all()])
+    proteindomain_id_to_name =  dict([(x.proteindomain_id, x.display_name) for x in nex_session.query(Proteindomain).all()])
     
     source_id = ipr.source_id
-    taxonomy_id = taxon.taxonomy_id
 
     key_to_annotation = {}
     for x in nex_session.query(Proteindomainannotation).filter_by(taxonomy_id=taxonomy_id).all():
@@ -46,60 +44,53 @@ def read_data_and_update_database(nex_session, fw):
 
     f = open(domain_file)
 
-    key_in_new = {}
-    
     i = 0
+    found = {}
     for line in f:
         items = line.strip().split("\t")
-        dbentity_id = name_to_dbentity_id.get(items[0])
+        IDs = items[0].split('_')
+        name = IDs[0]
+        dbentity_id = name_to_dbentity_id.get(name)
         if dbentity_id is None:
-            print("The systematic_name: ", items[0], " is not in the database.")
+            print("The systematic_name ", name, " is not in the LOCUSDBENTITY table.")
             continue
-        format_name = items[4].replace(' ', '_')
-        proteindomain_id = format_name_to_id.get(format_name)
+        domain_name = items[4].replace(' ', '_')
+        proteindomain_id = format_name_to_id.get(domain_name)
         if proteindomain_id is None:
-            print("The domain name:", format_name, " is not in the PROTEINDOMAIN table.")
+            print("The domain name:", domain_name, " is not in the PROTEINDOMAIN table.")
             continue
         start = int(items[6])
         end = int(items[7])
+        run_time = items[10].split('-')
+        run_date = run_time[2] + '-' + run_time[1] + '-' + run_time[0]
         key = (dbentity_id, proteindomain_id, start, end)
-        if key not in key_to_annotation:
-            print ("NEW: ", items[0], format_name, (dbentity_id, proteindomain_id, start, end))
+        if key not in key_to_annotation and key not in found:
             i = i + 1
-            insert_annotation(nex_session, fw, dbentity_id, proteindomain_id,
-                              source_id, taxonomy_id, start, end)
-            if i > 500:
-                nex_session.commit()
-                i = 0
-
-        key_in_new[key] = 1
-        
+            insert_annotation(nex_session, fw, name, domain_name, dbentity_id, proteindomain_id,
+                              source_id, taxonomy_id, start, end, run_date)
+            nex_session.commit()
+            # nex_session.rollback()
+            found[key] = 1
+        else:
+            key_to_annotation.pop(key)
     f.close()
 
-    
-    delete_annotations(nex_session, fw, key_to_annotation, key_in_new)
-
-    nex_session.commit()
-
-
-def delete_annotations(nex_session, fw, key_to_annotation, key_in_new):
-
-    i = 0
+    print("old_annotations_to_delete: ", len(list(key_to_annotation)))
     for key in key_to_annotation:
-        if key in key_in_new:
-            continue
-        i = i + 1
-        x = key_to_annotation[key]
-        nex_session.delete(x)
-        if i > 500:
-            nex_session.commit()
-            i = 0
-        fw.write("Delete old annotation for dbentity_id=" + str(x.dbentity_id) + ", proteindomain_id=" + str(x.proteindomain_id) + ", start_index=" + str(x.start_index) + ", end_index=" + str(x.end_index) + "\n")
-
+        (dbentity_id, proteindomain_id, start, end) = key
+        x = nex_session.query(Proteindomainannotation).filter_by(dbentity_id=dbentity_id,
+                                                                 proteindomain_id=proteindomain_id,
+                                                                 start_index=start,
+                                                                 end_index=end).one_or_none()
+        if x:
+            nex_session.delete(x)
+            print("Deleting old annotation for " + proteindomain_id_to_name[proteindomain_id] + ", dbentity_id=" + str(dbentity_id))
+            fw.write("Delete old ansnotation for dbentity_id=" + str(dbentity_id) + ", proteindomain_id=" + str(proteindomain_id) + ", start_index=" + str(start) + ", end_index=" + str(end) + "\n")
+    
     nex_session.commit()
-
-
-def insert_annotation(nex_session, fw, dbentity_id, proteindomain_id, source_id, taxonomy_id, start, end):
+    # nex_session.rollback()
+    
+def insert_annotation(nex_session, fw, name, domain_name, dbentity_id, proteindomain_id, source_id, taxonomy_id, start, end, run_date):
 
     x = Proteindomainannotation(dbentity_id = dbentity_id,
                                 taxonomy_id = taxonomy_id,
@@ -107,13 +98,12 @@ def insert_annotation(nex_session, fw, dbentity_id, proteindomain_id, source_id,
                                 proteindomain_id = proteindomain_id,
                                 start_index = start, 
                                 end_index = end,
-                                date_of_run = today,
+                                date_of_run = run_date,
                                 created_by = CREATED_BY)
 
     nex_session.add(x)
-    # nex_session.commit()
 
-    fw.write("Insert new annotation for dbentity_id=" + str(dbentity_id) + ", proteindomain_id=" + str(proteindomain_id) + ", start_index=" + str(start) + ", end_index=" + str(end) + "\n")
+    fw.write("Insert new ansnotation for " + name + " " + domain_name + ", dbentity_id=" + str(dbentity_id) + ", proteindomain_id=" + str(proteindomain_id) + ", start_index=" + str(start) + ", end_index=" + str(end) + "\n")
 
     
 if __name__ == "__main__":
