a681261b585ce41f3cdb56241a2d82b3e4d35ea2
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2023-07-19 16:56:39 -0700

    Redirect python stderr to stdout

--- system_config/cron/reference_update.sh
+++ system_config/cron/reference_update.sh
@@ -15,15 +15,15 @@ export AWS_ACCESS_KEY_ID=$ABC_AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$ABC_AWS_SECRET_ACCESS_KEY
 
 /usr/bin/cp /dev/null $LOG_FILE
-python /data/www/SGDBackend-Nex2/scripts/loading/reference/reference_update_from_abc.py | /usr/bin/tee -a $OUTPUT_FILE
+python /data/www/SGDBackend-Nex2/scripts/loading/reference/reference_update_from_abc.py 2>&1 | /usr/bin/tee -a $OUTPUT_FILE
 cat $LOG_FILE >> $OUTPUT_FILE
 
 /usr/bin/cp /dev/null $LOG_FILE
-python /data/www/SGDBackend-Nex2/scripts/loading/reference/reference_display_name_update.py | /usr/bin/tee -a $OUTPUT_FILE
+python /data/www/SGDBackend-Nex2/scripts/loading/reference/reference_display_name_update.py 2>&1 | /usr/bin/tee -a $OUTPUT_FILE
 cat $LOG_FILE >> $OUTPUT_FILE
 
 /usr/bin/cp /dev/null $LOG_FILE
-python /data/www/SGDBackend-Nex2/scripts/dumping/ncbi/dump_gene_pmid_pair.py | /usr/bin/tee -a $OUTPUT_FILE
+python /data/www/SGDBackend-Nex2/scripts/dumping/ncbi/dump_gene_pmid_pair.py 2>&1 | /usr/bin/tee -a $OUTPUT_FILE
 cat $LOG_FILE >> $OUTPUT_FILE
 
 echo "reference_update.sh finished:  `/bin/date`" | /bin/tee $OUTPUT_FILE
