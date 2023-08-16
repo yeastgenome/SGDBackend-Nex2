77f9e0305cc09ffaf7b2da2fa01e022162581e23
sweng66 <sweng@stanford.edu>
2023-08-16 13:30:05 -0700

    fixed to return both half-life unit and value

--- src/models.py
+++ src/models.py
@@ -5186,7 +5186,7 @@ class Locusdbentity(Dbentity):
         expt_data = DBSession.query(Proteinexptannotation).filter_by(dbentity_id=self.dbentity_id).all()
         if len(expt_data) > 0:
             obj["half_life"] = { 'data_value': str(expt_data[0].data_value),
-                                 'date_unit': str(expt_data[0].data_unit) }
+                                 'data_unit': str(expt_data[0].data_unit) }
 
         return obj
 
