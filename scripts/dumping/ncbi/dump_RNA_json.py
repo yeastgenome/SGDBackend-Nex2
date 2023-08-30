06af5327c9795adf258acf3cfd883d3ab6c12d88
sweng66 <sweng@stanford.edu>
2023-08-30 12:52:44 -0700

    fixed the release version

--- scripts/dumping/ncbi/dump_RNA_json.py
+++ scripts/dumping/ncbi/dump_RNA_json.py
@@ -30,7 +30,7 @@ s3_archive_dir = "curation/chromosomal_feature/"
 
 taxonId = "NCBITaxon:559292"
 TAXON = "TAX:559292"
-assembly = "R64-3-1"
+assembly = "R64-4-1"
 
 obsolete_pmids = ['PMID:25423496', 'PMID:27651461',
                   'PMID:28068213', 'PMID:31088842']
