f200aee89a71b35dbffd4b2d91b281c00cf73ff5
sweng66 <sweng@stanford.edu>
2024-03-14 14:30:24 -0700

    adding RNAcentral ID update script into cron

--- system_config/cron/dbxref_update.sh
+++ system_config/cron/dbxref_update.sh
@@ -4,4 +4,5 @@ cd /data/www/SGDBackend-NEX2/current
 source /data/envs/sgd3/bin/activate 
 source prod_variables.sh 
 python scripts/loading/dbxref/update_dbxref.py
+python scripts/loading/locus/load_RNAcentral_IDs.py
 
