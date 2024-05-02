1eada17ce9c99e05e934b63ff0dbf5f38de5a128
sweng66 <sweng@stanford.edu>
2024-05-02 14:52:33 -0700

    testing

--- scripts/search/es7_mapping.py
+++ scripts/search/es7_mapping.py
@@ -441,8 +441,12 @@ mapping = {
             },
             "labs": {
                 "type": "text",
-                "fielddata": True,
-                "analyzer": "keyword"
+                "fields": {
+                    "keyword": {
+                        "type": "keyword",
+                        "ignore_above": 256
+                    }
+                }
             },
             "phenotype_loci": {
                 "type": "text",
