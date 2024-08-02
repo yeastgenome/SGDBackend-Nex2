f494b7994f0118412ef5534c4ed196a2f3913aa6
sweng66 <sweng@stanford.edu>
2024-08-01 17:24:40 -0700

    fixed to output one line per complex

--- scripts/dumping/tab_files_for_download_site/generate_molecular_complex_file.py
+++ scripts/dumping/tab_files_for_download_site/generate_molecular_complex_file.py
@@ -10,20 +10,18 @@ complexFile = "scripts/dumping/tab_files_for_download_site/data/molecularComplex
 def dump_data():
 
     """
+    specs: one complex per row
+    
     EBI complex ID
     Complex Portal ID (CPX ID)
     Complex name
-    Complex Aliases
-    Complex Function summary
-    Complex Properties summary
-    Complex PubMed ID
-    Interactors Biological Role
-    Interactor Stoichiometry
-    Interactor Type
-    Interactor Secondary Id (systematic name)
-    Participant standard name (Standard Name)
-    Participant name
-    Participant SGDID
+    Complex systematic_name
+    Complex SGDID
+    Complex Aliases - pipe-separated list
+    Complex Description
+    Complex subunits - pipe-separated list alphabetical (use gene_name, if no gene_name use systematic_name
+    Subunit stoichiometry - pipe-separated list to match order of 'Complex subunits' row
+    Complex PubMed ID - pipe-separated list
     """
 
     print(datetime.now())
@@ -40,8 +38,9 @@ def dump_data():
     complex_id_to_aliases = {}
     for x in rows:
         aliases = complex_id_to_aliases.get(x[0], [])
-        aliases.append(x[1])
-        complex_id_to_aliases[x[0]] = aliases
+        if x[1] not in aliases:
+            aliases.append(x[1])
+            complex_id_to_aliases[x[0]] = aliases
     
     rows = nex_session.execute("SELECT cr.complex_id, rd.pmid "
                                "FROM nex.complex_reference cr, nex.referencedbentity rd "
@@ -50,49 +49,35 @@ def dump_data():
     for x in rows:
         pmids = complex_id_to_pmids.get(x[0], [])
         pmid = "PMID:" + str(x[1]) if x[1] else ""
-        if pmid:
+        if pmid and pmid not in pmids:
             pmids.append(pmid)
             complex_id_to_pmids[x[0]] = pmids
+                               
+    rows = nex_session.execute("SELECT cba.complex_id, cba.stoichiometry, i.format_name, i.display_name "  
+                               "FROM nex.complexbindingannotation cba, nex.interactor i "
+                               "WHERE cba.interactor_id = i.interactor_id").fetchall()
 
-    rows = nex_session.execute("SELECT cg.complex_id, g.display_name "
-                               "FROM nex.complex_go cg, nex.go g "
-                               "WHERE cg.go_id = g.go_id "
-                               "AND g.go_namespace = 'molecular function' "
-                               "ORDER BY 1, 2").fetchall()
-    complex_id_to_functions = {}
-    for x in rows:
-        functions = complex_id_to_functions.get(x[0], [])
-        functions.append(x[1])
-        complex_id_to_functions[x[0]] = functions
-    
-    rows = nex_session.execute("SELECT cba.complex_id, cba.stoichiometry, i.format_name, i.display_name, "
-                               "       i.locus_id, pt.display_name, pr.display_name "  
-                               "FROM nex.complexbindingannotation cba, nex.interactor i, "
-                               "     nex.psimi pt, nex.psimi pr "
-                               "WHERE cba.interactor_id = i.interactor_id "
-                               "AND i.type_id = pt.psimi_id "
-                               "AND i.role_id = pr.psimi_id").fetchall()
-
-    complex_id_to_interactors = {}
+    complex_id_to_stoichiometry_list = {}
+    complex_id_to_subunit_list = {}
     for x in rows:
         complex_id = x[0]
         stoichiometry = x[1] if x[1] else ""
-        format_name = x[2]
-        display_name = x[3]
-        locus_id = x[4]
-        type = x[5]
-        role = x[6]
-        sgdid = ""
-        systematic_name = ""
+        subunit = x[3] if x[3] else x[2]
+        
+        """
         if locus_id and locus_id in locus_id_to_data:
             (sgdid, systematic_name, gene_name, qualifier, genetic_position, desc) = locus_id_to_data[locus_id]
-        interactor = (stoichiometry, format_name, display_name, type, role, sgdid, systematic_name)
-        interactors = complex_id_to_interactors.get(complex_id, [])
-        interactors.append(interactor)
-        complex_id_to_interactors[complex_id] = interactors
-        
-    
-    rows = nex_session.execute("SELECT cd.intact_id, cd.complex_accession, d.display_name, cd.description, d.dbentity_id " 
+        """
+        subunit_list = complex_id_to_subunit_list.get(complex_id, [])
+        stoichiometry_list = complex_id_to_stoichiometry_list.get(complex_id, [])
+        if subunit not in subunit_list:
+            subunit_list.append(subunit)
+            stoichiometry_list.append(str(stoichiometry))
+            complex_id_to_subunit_list[complex_id] = subunit_list
+            complex_id_to_stoichiometry_list[complex_id] = stoichiometry_list
+
+    rows = nex_session.execute("SELECT cd.intact_id, cd.complex_accession, cd.systematic_name, "
+                               "       d.display_name, d.sgdid, cd.description, d.dbentity_id " 
                                "FROM nex.complexdbentity cd, nex.dbentity d "
                                "WHERE cd.dbentity_id = d.dbentity_id "
                                "ORDER BY 3").fetchall()
@@ -100,20 +85,19 @@ def dump_data():
     for x in rows:
         ebi_complex_id = x[0]
         cpx_id = x[1]
-        complexNm = x[2]
-        desc = x[3]
-        complex_id = x[4]
+        systematicNm = x[2]
+        complexNm = x[3]
+        sgdid = x[4]
+        desc = x[5]
+        complex_id = x[6]
         aliases = "|".join(complex_id_to_aliases.get(complex_id, []))
-        functions = "|".join(complex_id_to_functions.get(complex_id, []))
+        subunit_list = "|".join(complex_id_to_subunit_list.get(complex_id, []))
+        stoichiometry_list = "|".join(complex_id_to_stoichiometry_list.get(complex_id, []))
         pmids = "|".join(complex_id_to_pmids.get(complex_id, []))
-        interactors = complex_id_to_interactors.get(complex_id, [])
-        for interactor in interactors:
-            (stoichiometry, format_name, display_name, type, role, sgdid, systematic_name) = interactor
-            fw.write(ebi_complex_id + "\t" + cpx_id + "\t" + complexNm + "\t")
-            fw.write(aliases + "\t" + functions + "\t" + desc + "\t")
-            fw.write(pmids + "\t" + role + "\t" + str(stoichiometry) + "\t" + type + "\t")
-            fw.write(format_name + "\t" + display_name + "\t" + systematic_name + "\t")
-            fw.write(sgdid + "\n")
+        
+        fw.write(ebi_complex_id + "\t" + cpx_id + "\t" + complexNm + "\t")
+        fw.write(systematicNm + "\t" + sgdid + "\t" + aliases + "\t" + desc + "\t")
+        fw.write(subunit_list + "\t" + stoichiometry_list + "\t" + pmids + "\n")
         
     fw.close()
     nex_session.close()
