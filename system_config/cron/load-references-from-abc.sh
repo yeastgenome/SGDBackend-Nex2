93ba6ce2cb18e3540772650322c1a6e6c32e806a
stuartmiyasato <stuartmiyasato@users.noreply.github.com>
2024-12-03 15:28:12 -0800

    Wrapper script for loading reference abc files

---
+++ system_config/cron/load-references-from-abc.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+OUTPUT_FILE=/tmp/index_disambiguation.out
+
+echo "load-references-from-abc.sh start:  `/bin/date`" | /bin/tee $OUTPUT_FILE
+
+cd /data/www/SGDBackend-Nex2
+. venv/bin/activate
+
+echo "----------" | /bin/tee -a $OUTPUT_FILE
+echo "load_new_references_from_abc.py start:  `/bin/date`" | /bin/tee -a $OUTPUT_FILE
+python scripts/loading/reference/load_new_references_from_abc.py | /bin/tee -a $OUTPUT_FILE
+
+echo "----------" | /bin/tee -a $OUTPUT_FILE
+echo " load_tet_from_abc.py start:  `/bin/date`" | /bin/tee -a $OUTPUT_FILE
+python scripts/loading/reference/load_tet_from_abc.py | /bin/tee -a $OUTPUT_FILE
+
+echo "----------" | /bin/tee -a $OUTPUT_FILE
+echo "load-references-from-abc.sh end:  `/bin/date`" | /bin/tee -a $OUTPUT_FILE
+
+/usr/bin/aws sns publish \
+    --topic-arn $SNS_TOPIC_ARN \
+    --subject "load-references-from-abc.sh completed" \
+    --message file://${OUTPUT_FILE}
