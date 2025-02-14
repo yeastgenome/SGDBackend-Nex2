1fa6eadc2955276385aaa348b7511bb636482408
sweng66 <sweng@stanford.edu>
2025-02-13 21:03:31 -0800

    adding code to delete a reference

--- scripts/loading/reference/load_newly_deleted_references_from_abc.py
+++ scripts/loading/reference/load_newly_deleted_references_from_abc.py
@@ -1,11 +1,17 @@
 import sys
 import json
+import json
+from os import environ
 from urllib import request
 from scripts.loading.database_session import get_session
 from scripts.loading.reference.load_tet_from_abc import download_json_file
 from scripts.loading.reference.add_abc_reference import email_id_to_dbuser_mapping
-import json
-from os import environ
+from src.models import Referencedbentity, CuratorActivity, ReferenceFile, Referenceunlink, \
+    Referencetype, Referencedocument, Referenceauthor, ReferenceUrl, ReferenceAlias, \
+    StrainsummaryReference, Reservedname, PathwaysummaryReference, LocussummaryReference, \
+    LocusRelationReference, LocusnoteReference, LocusAliasReferences, LocusReferences, \
+    DatasetReference, CurationReference, ColleagueReference, Physinteractionannotation, \
+    Goannotation, Geninteractionannotation, Literatureannotation, Referencedeleted
 
 __author__ = 'sweng66'
 
@@ -38,7 +44,7 @@ def load_data():
         good_pmids_in_db[x[1]] = x[0]
 
     email_id_to_created_by = email_id_to_dbuser_mapping()
-    
+   
     for record in json_data['data']:
         pmid = int(record['pmid'].replace("PMID:", ""))
         email = record['updated_by_email']
@@ -54,18 +60,70 @@ def load_data():
         elif pmid in good_pmids_in_db:
             reference_id = good_pmids_in_db[pmid]
             # set it to 'Deleted' for now?
-            db.execute("UPDATE nex.dbentity SET dbentity_status = 'Deleted' WHERE dbentity_id = " + str(reference_id))
+            # db.execute("UPDATE nex.dbentity SET dbentity_status = 'Deleted' WHERE dbentity_id = " + str(reference_id))
             print("PMID:" + str(pmid) + ": Deleting from dbentity/referencedbentity and other ref related tables:", email, created_by)
-            db.rollback()
+            delete_reference(db, reference_id, pmid, created_by)
         else:
-            db.execute("INSERT INTO nex.referencedeleted (pmid, reason_deleted, created_by) "
-                       "VALUES (" + str(pmid) + ", 'This paper was deleted because the content is not relevant to S. cerevisiae.', '" + created_by + "')")
-            db.commit()
             print("PMID:" + str(pmid) + ": Inserting into referencedeleted table:", email, created_by)
+            x = Referencedeleted(pmid=pmid,
+                                 reason_deleted='This paper was deleted because the content is not relevant to S. cerevisiae.',
+                                 created_by=created_by)
+            db.add(x)
+            db.commit()
             
     db.close()
     print("DONE!")
     
+
+def delete_helper(db, reference_id, table, table_name):
+    count = db.query(table).filter_by(reference_id = reference_id).count()
+    if count > 0:
+        db.query(table).filter_by(reference_id = reference_id).delete()
+        print('{} records being deleted from {} for reference_id = {}'.format(count, table_name, reference_id))
+
+
+def delete_reference(db, reference_id, pmid, created_by):
+
+    try:
+        delete_helper(db, reference_id, Literatureannotation, 'Literatureannotation')
+        delete_helper(db, reference_id, Geninteractionannotation, 'Geninteractionannotation')
+        delete_helper(db, reference_id, Goannotation, 'Goannotation')
+        delete_helper(db, reference_id, Physinteractionannotation, 'Physinteractionannotation')
+        delete_helper(db, reference_id, ColleagueReference, 'ColleagueReference')
+        delete_helper(db, reference_id, CurationReference, 'CurationReference')
+        delete_helper(db, reference_id, DatasetReference, 'DatasetReference')
+        delete_helper(db, reference_id, DatasetReference, 'DatasetReference')
+        delete_helper(db, reference_id, LocusReferences, 'LocusReferences')
+        delete_helper(db, reference_id, LocusAliasReferences, 'LocusAliasReferences')
+        delete_helper(db, reference_id,	LocusnoteReference, 'LocusnoteReference')
+        delete_helper(db, reference_id, LocusRelationReference, 'LocusRelationReference')
+        delete_helper(db, reference_id,	LocussummaryReference, 'LocussummaryReference')
+        delete_helper(db, reference_id,	PathwaysummaryReference, 'PathwaysummaryReference')
+        delete_helper(db, reference_id,	Reservedname, 'Reservedname')
+        delete_helper(db, reference_id, StrainsummaryReference, 'StrainsummaryReference')
+        delete_helper(db, reference_id, ReferenceAlias, 'ReferenceAlias')
+        delete_helper(db, reference_id,	ReferenceUrl, 'ReferenceUrl')
+        delete_helper(db, reference_id, Referenceauthor, 'Referenceauthor')
+        delete_helper(db, reference_id, Referencedocument, 'Referencedocument')
+        delete_helper(db, reference_id,	Referencetype, 'Referencetype')
+        delete_helper(db, reference_id, Referenceunlink, 'Referenceunlink')
+        delete_helper(db, reference_id,	ReferenceFile, 'ReferenceFile')
+        ### delete rows in CuratorActivity if there are any
+        count = db.query(CuratorActivity).filter_by(dbentity_id = reference_id).count()
+        if count > 0:
+            db.query(CuratorActivity).filter_by(dbentity_id = reference_id).delete()
+            print('{} records being deleted from {} for reference_id = {}'.format(count, "CuratorActivity", reference_id))
+        reference = db.query(Referencedbentity).filter_by(dbentity_id=reference_id).one_or_none()
+        db.delete(reference)
+        print('{} records being deleted from {} for reference_id = {}'.format(count, "Dbentity/Referencedbentity", reference_id))
+        x = Referencedeleted(pmid=pmid,
+                             reason_deleted='This paper was deleted by ' + created_by,
+                             created_by=created_by)
+        db.add(x)
+        db.commit()
+    except Exception as e:
+        print("Error deleting PMID:" + str(pmid) + ": " + str(e))
+        
 
 if __name__ == '__main__':
 
