9386de26eeaf82772a78026f90a9b202d09c75f4
sweng66 <sweng@stanford.edu>
2025-07-31 15:33:36 -0700

    fixed the cutoff for noctua gpad file

--- scripts/loading/go/load_gpad.py
+++ scripts/loading/go/load_gpad.py
@@ -777,7 +777,7 @@ if __name__ == "__main__":
     file_sizes = {
         dated_gpad_file: 1100000,
         dated_gpi_file: 340000,
-        dated_noctua_gpad_file: 790000,
+        dated_noctua_gpad_file: 780000,
         dated_complex_gpad_file: 2700000
     }
     
