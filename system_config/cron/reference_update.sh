97b5194ba9725091c2d300422d7e0c20a2996602
sweng66 <sweng@stanford.edu>
2026-02-19 22:44:31 -0800

    fixed to use relative path

--- system_config/cron/reference_update.sh
+++ system_config/cron/reference_update.sh
@@ -1,24 +1,16 @@
-#!/bin/sh
-set -e
+#! /bin/sh
 
 cd /data/www/SGDBackend-Nex2
-. venv/bin/activate
-. ./prod_variables.sh
-
-# set credentials to access ABC
-export AWS_ACCESS_KEY_ID=$ABC_AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$ABC_AWS_SECRET_ACCESS_KEY
+source venv/bin/activate 
+source prod_variables.sh
 
 export PYTHONPATH=/data/www/SGDBackend-Nex2
 
 LOG=/data/www/logs/reference_update.log
 
-python /data/www/SGDBackend-Nex2/scripts/loading/reference/reference_update_from_abc.py > "$LOG" 2>&1
-# python /data/www/SGDBackend-Nex2/scripts/loading/reference/reference_update_non_pubmed_from_abc.py >> "$LOG" 2>&1
+python scripts/loading/reference/reference_update_from_abc.py > "$LOG" 2>&1
+# python scripts/loading/reference/reference_update_non_pubmed_from_abc.py >> "$LOG" 2>&1
+python scripts/loading/reference/reference_display_name_update.py >> "$LOG" 2>&1
+python scripts/dumping/ncbi/dump_gene_pmid_pair.py >> "$LOG" 2>&1
 
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
-
-python /data/www/SGDBackend-Nex2/scripts/loading/reference/reference_display_name_update.py >> "$LOG" 2>&1
-python /data/www/SGDBackend-Nex2/scripts/dumping/ncbi/dump_gene_pmid_pair.py >> "$LOG" 2>&1
 
